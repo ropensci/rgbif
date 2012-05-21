7ab3e81c9e4139c954ea8799729ea352907b0e9e
VijayBarve <vjaybarve@rediffmail.com>
2012-05-21 17:05:18 -0500

    Latitude, Longitude, Altitude and Depth parameters added

--- R/occurrencelist.R
+++ R/occurrencelist.R
@@ -7,41 +7,64 @@
 #'@param  dataresourcekey Filter records to those provided by the supplied
 #'    numeric key for a data resource See resource(). (character)
 #'@param  resourcenetworkkey  <what param does>
-#'@param  basisofrecordcode  <what param does>
-#'@param  minlatitude  <what param does>
-#'@param  maxlatitude  <what param does>
-#'@param  minlongitude  <what param does>
-#'@param  maxlongitude  <what param does>
-#'@param  minaltitude  <what param does>
-#'@param  maxaltitude  <what param does>
-#'@param  mindepth  <what param does>
-#'@param  maxdepth  <what param does>
-#'@param  cellid  <what param does>
-#'@param  centicellid  <what param does>
-#'@param  typesonly  <what param does>
-#'@param  georeferencedonly  <what param does>
-#'@param  coordinatestatus  <what param does>
-#'@param  coordinateissues  <what param does>
-#'@param  hostisocountrycode  <what param does>
-#'@param  originisocountrycode  <what param does>
-#'@param  originregioncode  <what param does>
-#'@param  startdate  <what param does>
-#'@param  enddate  <what param does>
-#'@param  startyear  <what param does>
-#'@param  endyear  <what param does>
-#'@param  year  <what param does>
-#'@param  month  <what param does>
-#'@param  day  <what param does>
-#'@param modifiedsince  <what param does>
-#'@param  startindex  <what param does>
-#'@param  maxresults  max number of results
+#'@param  basisofrecordcode  return only records with the specified basis of record.
+#'    Supported values are: "specimen, observation, living, germplasm, fossil, unknown".
+#'    (character)
+#'@param  minlatitude  return only records from locations with latitudes greater 
+#'    than the supplied value (southern hemisphere with negative latitudes). (numeric)
+#'@param  maxlatitude  return only records from locations with latitudes lower than 
+#'    the supplied value (southern hemisphere with negative latitudes). (numeric)
+#'@param  minlongitude  return only records from locations with longitudes greater 
+#'    than the supplied value (western hemisphere with negative longitudes). (numeric)
+#'@param  maxlongitude  return only records from locations with longitudes lower 
+#'    than the supplied value (western hemisphere with negative longitudes). (numeric)
+#'@param  minaltitude  return only records from altitudes greater than or equal to 
+#'    the supplied value. (integer)
+#'@param  maxaltitude  return only records from altitudes less than or equals to 
+#'    the supplied value. (integer)
+#'@param  mindepth  return only records from depth greater than or equal to the supplied 
+#'    value. (numeric 2 decimal places)
+#'@param  maxdepth  return only records from depth less than or equals to the supplied 
+#'    value. (numeric 2 decimal places)
+#'@param  cellid  identifier for a one degree cell (O - 64,799)
+#'@param  centicellid  identifier for a 0.1 degree cell within a one degree cell 
+#'@param  typesonly  if set to "true", return only records with a type status specified.
+#'@param  georeferencedonly  This option is deprecated.
+#'@param  coordinatestatus  if set to "true", return only records with coordinates. 
+#'    If set to "false", return only records without coordinates.
+#'@param  coordinateissues  if set to "true", return only records for which the portal 
+#'    has detected possible issues in georeferencing. If set to "false", return only 
+#'    records for which the portal has not detected any such issues.
+#'@param  hostisocountrycode  return only records served by providers from the country 
+#'    identified by the supplied 2-letter ISO code.
+#'@param  originisocountrycode return only records of occurrences which occurred 
+#'    within the country identified by the supplied 2-letter ISO code. 
+#'@param  originregioncode  return only records of occurrences which occurred 
+#'    within the region identified by the supplied 3-letter code.
+#'@param  startdate  return only records occurring on or after the supplied date 
+#'    (format YYYY-MM-DD, e.g. 2006-11-28).
+#'@param  enddate  return only records occurring on or before the supplied date 
+#'    (format YYYY-MM-DD, e.g. 2006-11-28).
+#'@param  startyear  return only records from during or after the supplied year.
+#'@param  endyear  return only records from during or before the supplied year.
+#'@param  year  return only records from during the supplied year.
+#'@param  month  return only records from during the supplied month (expressed as 
+#'    an integer in the range 1 to 12).
+#'@param  day  return only records from during the supplied day of month 
+#'    (expressed as an integer in the range 1 to 31).
+#'@param modifiedsince  return only records which have been indexed or modified 
+#'    in the GBIF data portal index on or after the supplied date 
+#'    (format YYYY-MM-DD, e.g. 2006-11-28). 
+#'@param  startindex  return the subset of the matching records that starts at 
+#'    the supplied (zero-based index). 
+#'@param  maxresults  max number of results (integer) (1-10000)
 #'@param  format  specifies the format in which the records are to be returned,
 #     one of: brief, darwin or kml (character)
 #'@param  icon  <what param does>
 #'@param mode  specifies whether the response data should (as far as possible)  be the raw values originally retrieved from the data resource or processed (normalised) values used within the data portal (character)latlongdf: return a data.frame of lat/long's for all occurrences (logical)
 #'@param  stylesheet sets the URL of the stylesheet to be associated with the
 #     response document.
-#'@param  latlongdf  <what param does>
+#'@param  latlongdf  Return results of function as data.frame
 #'@param url the base GBIF API url for the function (should be left to default)
 #'@param ... optional additional curl options (debugging tools mostly)
 #'@param curl If using in a loop, call getCurlHandle() first and pass
@@ -68,19 +91,68 @@ occurrencelist <- function(sciname = NA, taxonconceptKey = NA,
     } else {
         sciname2 <- NULL
     }
+    if (!is.na(basisofrecordcode)) {
+      basisofrecordcode2 <- paste("&basisofrecordcode=", basisofrecordcode, sep = "")
+    } else {
+      basisofrecordcode2 <- NULL
+    }
     if (!is.na(coordinatestatus)) {
         coordinatestatus2 <- paste("&coordinatestatus=", coordinatestatus,
             sep = "")
     } else {
         coordinatestatus2 <- NULL
     }
+    if (!is.na(minlatitude)) {
+      minlatitude2 <- paste("&minlatitude=", minlatitude, sep = "")
+    } else {
+      minlatitude2 <- NULL
+    }
+    if (!is.na(maxlatitude)) {
+      maxlatitude2 <- paste("&maxlatitude=", maxlatitude, sep = "")
+    } else {
+      maxlatitude2 <- NULL
+    }
+    if (!is.na(minlongitude)) {
+      minlongitude2 <- paste("&minlongitude=", minlongitude, sep = "")
+    } else {
+      minlongitude2 <- NULL
+    }
+    if (!is.na(maxlongitude)) {
+      maxlongitude2 <- paste("&maxlongitude=", maxlongitude, sep = "")
+    } else {
+      maxlongitude2 <- NULL
+    }
+    if (!is.na(minaltitude)) {
+      minaltitude2 <- paste("&minaltitude=", minaltitude, sep = "")
+    } else {
+      minaltitude2 <- NULL
+    }
+    if (!is.na(maxaltitude)) {
+      maxaltitude2 <- paste("&maxaltitude=", maxaltitude, sep = "")
+    } else {
+      maxaltitude2 <- NULL
+    }
+    if (!is.na(mindepth)) {
+      mindepth2 <- paste("&mindepth=", mindepth, sep = "")
+    } else {
+      mindepth2 <- NULL
+    }
+    if (!is.na(maxdepth)) {
+      maxdepth2 <- paste("&maxdepth=", maxdepth, sep = "")
+    } else {
+      maxdepth2 <- NULL
+    }
     if (!is.na(maxresults)) {
         maxresults2 <- paste("&maxresults=", maxresults, sep = "")
     } else {
         maxresults2 <- NULL
     }
-    args <- paste(sciname2, maxresults2, coordinatestatus2, sep = "")
-    query <- paste(url, args, sep = "")
+
+    args <- paste(sciname2, basisofrecordcode2, maxresults2, coordinatestatus2, 
+                  minlatitude2, maxlatitude2, minlongitude2, maxlongitude2, 
+                  minaltitude2, maxaltitude2, mindepth2, maxdepth2, sep = "")
+    
+    query <<- paste(url, args, sep = "")
     tt <- getURL(query, ..., curl = curl)
     out <- xmlTreeParse(tt)$doc$children$gbifResponse
     if (latlongdf == TRUE) {
