e093abd667206c0d5bcb53eda1e88482dd1408ba
VijayBarve <vjaybarve@rediffmail.com>
2012-06-17 17:42:59 -0500

    Updating man pages

--- R/occurrencelist.R
+++ R/occurrencelist.R
@@ -1,76 +1,76 @@
 #'occurrencelist description
 #'@import RCurl XML plyr
-#'@param  sciname scientitic name of taxon (character, see example)
-#'@param  taxonconceptKey unique key for taxon (numeric)
-#'@param  dataproviderkey Filter records to those provided by the supplied
+#' @param  sciname scientitic name of taxon (character, see example)
+#' @param  taxonconceptKey unique key for taxon (numeric)
+#' @param  dataproviderkey Filter records to those provided by the supplied
 #'    numeric key for a data provider. See provider(). (character)
-#'@param  dataresourcekey Filter records to those provided by the supplied
+#' @param  dataresourcekey Filter records to those provided by the supplied
 #'    numeric key for a data resource See resource(). (character)
-#'@param  resourcenetworkkey  <what param does>
-#'@param  basisofrecordcode  return only records with the specified basis of record.
+#' @param  resourcenetworkkey  <what param does>
+#' @param  basisofrecordcode  return only records with the specified basis of record.
 #'    Supported values are: "specimen, observation, living, germplasm, fossil, unknown".
 #'    (character)
-#'@param  minlatitude  return only records from locations with latitudes greater 
+#' @param  minlatitude  return only records from locations with latitudes greater 
 #'    than the supplied value (southern hemisphere with negative latitudes). (numeric)
-#'@param  maxlatitude  return only records from locations with latitudes lower than 
+#' @param  maxlatitude  return only records from locations with latitudes lower than 
 #'    the supplied value (southern hemisphere with negative latitudes). (numeric)
-#'@param  minlongitude  return only records from locations with longitudes greater 
+#' @param  minlongitude  return only records from locations with longitudes greater 
 #'    than the supplied value (western hemisphere with negative longitudes). (numeric)
-#'@param  maxlongitude  return only records from locations with longitudes lower 
+#' @param  maxlongitude  return only records from locations with longitudes lower 
 #'    than the supplied value (western hemisphere with negative longitudes). (numeric)
-#'@param  minaltitude  return only records from altitudes greater than or equal to 
+#' @param  minaltitude  return only records from altitudes greater than or equal to 
 #'    the supplied value. (integer)
-#'@param  maxaltitude  return only records from altitudes less than or equals to 
+#' @param  maxaltitude  return only records from altitudes less than or equals to 
 #'    the supplied value. (integer)
-#'@param  mindepth  return only records from depth greater than or equal to the supplied 
+#' @param  mindepth  return only records from depth greater than or equal to the supplied 
 #'    value. (numeric 2 decimal places)
-#'@param  maxdepth  return only records from depth less than or equals to the supplied 
+#' @param  maxdepth  return only records from depth less than or equals to the supplied 
 #'    value. (numeric 2 decimal places)
-#'@param  cellid  identifier for a one degree cell (O - 64,799)
-#'@param  centicellid  identifier for a 0.1 degree cell within a one degree cell 
-#'@param  typesonly  if set to "true", return only records with a type status specified.
-#'@param  georeferencedonly  This option is deprecated.
-#'@param  coordinatestatus  if set to "true", return only records with coordinates. 
+#' @param  cellid  identifier for a one degree cell (O - 64,799)
+#' @param  centicellid  identifier for a 0.1 degree cell within a one degree cell 
+#' @param  typesonly  if set to "true", return only records with a type status specified.
+#' @param  georeferencedonly  This option is deprecated.
+#' @param  coordinatestatus  if set to "true", return only records with coordinates. 
 #'    If set to "false", return only records without coordinates.
-#'@param  coordinateissues  if set to "true", return only records for which the portal 
+#' @param  coordinateissues  if set to "true", return only records for which the portal 
 #'    has detected possible issues in georeferencing. If set to "false", return only 
 #'    records for which the portal has not detected any such issues.
-#'@param  hostisocountrycode  return only records served by providers from the country 
+#' @param  hostisocountrycode  return only records served by providers from the country 
 #'    identified by the supplied 2-letter ISO code.
-#'@param  originisocountrycode return only records of occurrences which occurred 
+#' @param  originisocountrycode return only records of occurrences which occurred 
 #'    within the country identified by the supplied 2-letter ISO code. 
-#'@param  originregioncode  return only records of occurrences which occurred 
+#' @param  originregioncode  return only records of occurrences which occurred 
 #'    within the region identified by the supplied 3-letter code.
-#'@param  startdate  return only records occurring on or after the supplied date 
+#' @param  startdate  return only records occurring on or after the supplied date 
 #'    (format YYYY-MM-DD, e.g. 2006-11-28).
-#'@param  enddate  return only records occurring on or before the supplied date 
+#' @param  enddate  return only records occurring on or before the supplied date 
 #'    (format YYYY-MM-DD, e.g. 2006-11-28).
-#'@param  startyear  return only records from during or after the supplied year.
-#'@param  endyear  return only records from during or before the supplied year.
-#'@param  year  return only records from during the supplied year.
-#'@param  month  return only records from during the supplied month (expressed as 
+#' @param  startyear  return only records from during or after the supplied year.
+#' @param  endyear  return only records from during or before the supplied year.
+#' @param  year  return only records from during the supplied year.
+#' @param  month  return only records from during the supplied month (expressed as 
 #'    an integer in the range 1 to 12).
-#'@param  day  return only records from during the supplied day of month 
+#' @param  day  return only records from during the supplied day of month 
 #'    (expressed as an integer in the range 1 to 31).
-#'@param modifiedsince  return only records which have been indexed or modified 
+#' @param modifiedsince  return only records which have been indexed or modified 
 #'    in the GBIF data portal index on or after the supplied date 
 #'    (format YYYY-MM-DD, e.g. 2006-11-28). 
-#'@param  startindex  return the subset of the matching records that starts at 
+#' @param  startindex  return the subset of the matching records that starts at 
 #'    the supplied (zero-based index). 
-#'@param  maxresults  max number of results (integer) (1-10000)
-#'@param  format  specifies the format in which the records are to be returned,
+#' @param  maxresults  max number of results (integer) (1-10000)
+#' @param  format  specifies the format in which the records are to be returned,
 #     one of: brief, darwin or kml (character)
-#'@param  icon  <what param does>
-#'@param mode  specifies whether the response data should (as far as possible)  
+#' @param  icon  <what param does>
+#' @param mode  specifies whether the response data should (as far as possible)  
 #'    be the raw values originally retrieved from the data resource or processed 
 #'    (normalised) values used within the data portal (character)
-#'@param  stylesheet sets the URL of the stylesheet to be associated with the
+#' @param  stylesheet sets the URL of the stylesheet to be associated with the
 #     response document.
-#'@param  latlongdf  return a data.frame of lat/long's for all occurrences (logical)
-#'@param  removeZeros remove records with both Lat Long zero values (logical) 
-#'@param url the base GBIF API url for the function (should be left to default)
-#'@param ... optional additional curl options (debugging tools mostly)
-#'@param curl If using in a loop, call getCurlHandle() first and pass
+#' @param  latlongdf  return a data.frame of lat/long's for all occurrences (logical)
+#' @param  removeZeros remove records with both Lat Long zero values (logical) 
+#' @param url the base GBIF API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass
 #' the returned value in here (avoids unnecessary footprint)
 #'@export
 #'@examples \dontrun{
@@ -307,7 +307,7 @@ occurrencelist <- function(sciname = NA, taxonconceptKey = NA,
                 startindex2, format2, icon2, mode2, stylesheet2, sep = "")
   if(!noCount){
   urlct = "http://data.gbif.org/ws/rest/occurrence/count?"
-  queryct <<- paste(urlct, args, sep = "")
+  queryct <- paste(urlct, args, sep = "")
   x <- try(readLines(queryct, warn = FALSE))
   x <- x[grep("totalMatched", x)]
   n <- as.integer(unlist(strsplit(x, "\""))[2])
@@ -316,7 +316,7 @@ occurrencelist <- function(sciname = NA, taxonconceptKey = NA,
     return(invisible(NULL))
   }
   }
-  query <<- paste(url, args, sep = "")
+  query <- paste(url, args, sep = "")
   tt <- getURL(query, ..., curl = curl)
   #tt <- getURL(query, curl = curl)
   
