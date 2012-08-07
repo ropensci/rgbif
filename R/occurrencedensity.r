23e0e46a25c9fa948e8eea1e634d03d01b72030b
Scott Chamberlain <myrmecocystus@gmail.com>
2012-08-07 08:04:40 -0700

    added option to write a csv file in the occurrencelist function

--- R/occurrencedensity.r
+++ R/occurrencedensity.r
@@ -1,26 +1,28 @@
-#'returns summary counts of occurrence records by one-degree cell for a single
-#' taxon, country, dataset, data publisher or data network
-#'@import RCurl XML
-#'@param taxonconceptkey numeric key uniquely identifying the taxon
-#'@param dataproviderkey numeric key uniquely identifying the data provider
-#'@param dataresourcekey numeric key uniquely identifying the data resource
-#'@param resourcenetworkkey numeric key uniquely identifying the data network
-#'@param originisocountrycode numeric key uniquely identifying the country
-#'@param format specifies the format in which the records are to be returned,
-#' one of: brief, darwin or kml (character)
-#'@param url the base GBIF API url for the function (should be left to default)
-#'@param ... optional additional curl options (debugging tools mostly)
-#'@param curl If using in a loop, call getCurlHandle() first and pass
-#' the returned value in here (avoids unnecessary footprint)
-#'@export
-#'@examples \dontrun{
-#'occurrencedensity(originisocountrycode='CA')
-#'occurrencedensity(taxonconceptkey=45)
-#'}
+#' Returns summary counts of occurrence records by one-degree cell for a single
+#' 		taxon, country, dataset, data publisher or data network.
+#' 		
+#' @import RCurl XML
+#' @param taxonconceptkey numeric key uniquely identifying the taxon
+#' @param dataproviderkey numeric key uniquely identifying the data provider
+#' @param dataresourcekey numeric key uniquely identifying the data resource
+#' @param resourcenetworkkey numeric key uniquely identifying the data network
+#' @param originisocountrycode numeric key uniquely identifying the country
+#' @param format specifies the format in which the records are to be returned,
+#' 		one of: brief, darwin or kml (character)
+#' @param url the base GBIF API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass
+#' 		the returned value in here (avoids unnecessary footprint)
+#' @examples \dontrun{
+#' occurrencedensity(originisocountrycode='CA')
+#' occurrencedensity(taxonconceptkey=45)
+#' }
+#' @export
 occurrencedensity <- function(taxonconceptkey = NA,
     dataproviderkey = NA, dataresourcekey = NA, resourcenetworkkey = NA,
     originisocountrycode = NA, format = NA, url = "http://data.gbif.org/ws/rest/density/list",
-    ..., curl = getCurlHandle()) {
+    ..., curl = getCurlHandle()) 
+{
     args <- list()
     if (!is.na(taxonconceptkey))
         args$taxonconceptkey <- taxonconceptkey
