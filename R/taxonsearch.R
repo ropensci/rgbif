eca3071a240449778d71fce88bc3ca1b966c177a
Scott Chamberlain <myrmecocystus@gmail.com>
2012-10-03 23:18:43 -0700

    small edit to taxonsearch and to README

--- R/taxonsearch.R
+++ R/taxonsearch.R
@@ -1,15 +1,15 @@
-#'Search for taxa in GBIF
+#' Search for taxa in GBIF
 #'
-#'Search for a taxon using scientific name. Optionally, include taxonomic
-#'rank in the search.
+#' Search for a taxon using scientific name. Optionally, include taxonomic
+#' rank in the search.
 #'
-#'Returns list of TaxonConcept key values
+#' Returns list of TaxonConcept key values
 #'
-#'@import XML RCurl
-#'@param sciname  scientitic name of taxon (character, see example)
-#'@param rank  rank of taxon, see taxrank() (character)
-#'@param maxresults  return at most the specified number of records. The
-#'   default (and maximum supported) is 1000 records.
+#' @import XML RCurl
+#' @param sciname  scientitic name of taxon (character, see example)
+#' @param rank  rank of taxon, see taxrank() (character)
+#' @param maxresults  return at most the specified number of records. The
+#'   	default (and maximum supported) is 1000 records.
 #' @param  dataproviderkey Filter records to those provided by the supplied
 #'    numeric key for a data provider. See provider(). (character)
 #' @param  dataresourcekey Filter records to those provided by the supplied
@@ -20,19 +20,20 @@
 #'    identified by the supplied 2-letter ISO code.
 #' @param  startindex  return the subset of the matching records that starts at 
 #'    the supplied (zero-based index). 
-#'@param url the base GBIF API url for the function (should be left to default)
-#'@param ... optional additional curl options (debugging tools mostly)
-#'@param curl If using in a loop, call getCurlHandle() first and pass
-#' the returned value in here (avoids unnecessary footprint)
-#'@export
-#'@examples \dontrun{
-#'taxonsearch(sciname = 'Puma concolor')
-#'}
+#' @param url the base GBIF API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass
+#' 		the returned value in here (avoids unnecessary footprint)
+#' @export
+#' @examples \dontrun{
+#' taxonsearch(sciname = 'Puma concolor')
+#' }
 taxonsearch <- function(sciname = NA, rank = NA, maxresults = 10,
                         dataproviderkey = NA, dataresourcekey = NA, resourcenetworkkey = NA,
                         hostisocountrycode = NA, startindex = NA,
                         url = "http://data.gbif.org/ws/rest/taxon/list?",
-                        ..., curl = getCurlHandle()) {
+                        ..., curl = getCurlHandle()) 
+{
   if (!is.na(sciname)) {
     sciname2 <- paste("scientificname=", sub(" ", "+", sciname),
                       sep = "")
@@ -86,4 +87,3 @@ taxonsearch <- function(sciname = NA, rank = NA, maxresults = 10,
   }
   out
 }
-# out <- taxonsearch(sciname = 'Accipiter erythronemius', maxresults = 1000)
--- README.md
+++ README.md
@@ -17,7 +17,7 @@ install_github("rgbif", "ropensci")
 require(rgbif)
 ```
 
-This set of functions/package will access data from [GBIF](http://opensnp.org/) using their API methods. 
+This set of functions/package will access data from [GBIF](http://www.gbif.org/) using their API methods. 
 
 See documentation the GBIF API here:  
 http://data.gbif.org/tutorial/services
