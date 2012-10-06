9cecb173b88f256c154882a2f4b17c2354f782e2
Scott Chamberlain <myrmecocystus@gmail.com>
2012-10-05 23:18:06 -0700

    added fxn taxonget to get taxonomic info on any species using a gbif key

--- R/taxonget.r
+++ R/taxonget.r
@@ -2,22 +2,26 @@
 #' 		concept keys.
 #'
 #' @import httr XML plyr
-#' @param key Key for a taxon.
-#' @return Data.frame of taxonomic information.
+#' @param key A single key, or many keys in a vector, for a taxon.
+#' @param url the base GBIF API url for the function (leave to default).
+#' @return A single data.frame of taxonomic information if  single data.frame is
+#' 		supplied, or a list of data.frame's if a list of keys is supplied.
 #' @examples \dontrun{
 #' keys <- taxonsearch(scientificname = 'Puma concolor')
-#' taxonget(key = keys[[1]])
+#' taxonget(keys)
 #' }
 #' @export
-taxonget <- function(key = NULL,
-   url = "http://data.gbif.org/ws/rest/taxon/get") 
+taxonget <- function(key = NULL, url = "http://data.gbif.org/ws/rest/taxon/get") 
 {
-	args <- compact(list(key = key))
-	temp <- GET(url, query = args)
-	out <- content(temp, as="text")
-	tt <- xmlParse(out)	
-	gbifkeys <- sapply(getNodeSet(tt, "//tc:TaxonConcept[@gbifKey]"), xmlGetAttr, "gbifKey")
-	sciname <- sapply(getNodeSet(tt, "//tn:nameComplete"), xmlValue)
-	rank <- sapply(getNodeSet(tt, "//tn:rankString"), xmlValue)
-	data.frame(sciname, gbifkeys, rank)	
+	doit <- function(x) {
+		args <- compact(list(key = x))
+		temp <- GET(url, query = args)
+		out <- content(temp, as="text")
+		tt <- xmlParse(out)	
+		gbifkeys <- sapply(getNodeSet(tt, "//tc:TaxonConcept[@gbifKey]"), xmlGetAttr, "gbifKey")
+		sciname <- sapply(getNodeSet(tt, "//tn:nameComplete"), xmlValue)
+		rank <- sapply(getNodeSet(tt, "//tn:rankString"), xmlValue)
+		data.frame(sciname, gbifkeys, rank)
+	}
+	lapply(key, doit)
 }
