87197c18d6d57f12e0f0a8a0525f11dddc715eca
Scott Chamberlain <myrmecocystus@gmail.com>
2012-12-09 15:56:11 -0800

    updating occurrencelist to get > 1000 records

--- R/occurrencelist.R
+++ R/occurrencelist.R
@@ -1,6 +1,6 @@
 #' Occurrencelist searches for taxon concept records matching a range of filters.
 #'
-#' @import RCurl XML plyr
+#' @import RCurl XML plyr doMC
 #' @param  scientificname scientitic name of taxon (character, see example)
 #' @param  taxonconceptKey unique key for taxon (numeric)
 #' @param  dataproviderkey Filter records to those provided by the supplied
@@ -81,13 +81,23 @@
 #' @param curl If using in a loop, call getCurlHandle() first and pass
 #' the returned value in here (avoids unnecessary footprint)
 #' @examples \dontrun{
-#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100, latlongdf = T)
-#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100, latlongdf=F)
-#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100, latlongdf=T, writecsv="~/myyyy.csv")
+#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100)
+#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 5)
+#' occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100, writecsv="~/myyyy.csv")
 #' 
 #' # Many species
+#' library(doMC)
+#' registerDoMC(cores=4)
 #' splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa')
-#' out <- lapply(splist, function(x) occurrencelist(x, coordinatestatus = T, maxresults = 100, latlongdf = T))
+#' out <- llply(splist, function(x) occurrencelist(x, coordinatestatus = T, maxresults = 100), .parallel=T)
+#' lapply(out, head)
+#' 
+#' # In parallel, register a parallel backend according to your OS, on Mac I use doMC
+#' system.time( occurrencelist(scientificname = 'Erebia gorge*', coordinatestatus = TRUE, maxresults = 2000, parallel=F) )
+#' 
+#' library(doMC)
+#' registerDoMC(cores=4)
+#' system.time( occurrencelist(scientificname = 'Erebia gorge*', coordinatestatus = TRUE, maxresults = 2000, parallel=T) )
 #' }
 #' @export
 occurrencelist <- function(scientificname = NULL, taxonconceptKey = NULL,
@@ -101,7 +111,7 @@ occurrencelist <- function(scientificname = NULL, taxonconceptKey = NULL,
 		originregioncode = NULL, startdate = NULL, enddate = NULL, startyear = NULL,
 		endyear = NULL, year = NULL, month = NULL, day = NULL, modifiedsince = NULL,
 		startindex = NULL, maxresults = 10, format = NA, icon = NULL,
-		mode = NULL, stylesheet = NULL, latlongdf = FALSE, removeZeros = FALSE, writecsv = NULL,
+		mode = NULL, stylesheet = NULL, removeZeros = FALSE, writecsv = NULL, parallel = FALSE,
 		url = "http://data.gbif.org/ws/rest/occurrence/list", ..., curl = getCurlHandle()) 
 {	
 	args <- compact(
@@ -121,24 +131,44 @@ occurrencelist <- function(scientificname = NULL, taxonconceptKey = NULL,
 			modifiedsince=modifiedsince, startindex=startindex, format=format,
 			icon=icon, mode=mode, stylesheet=stylesheet, maxresults=maxresults
 		))
-	tt <- getForm(url, .params = args, curl = curl)
-	out <- xmlParse(tt)
-	if (latlongdf == TRUE) {
-		df <- gbifxmlToDataFrame(out, format)
+	counted <- getForm("http://data.gbif.org/ws/rest/occurrence/count", .params = args, curl = curl)
+	num <- as.numeric(xmlGetAttr(getNodeSet(xmlParse(counted), "//gbif:summary", namespaces="gbif")[[1]], "totalMatched"))
+	num <- min(c(num, maxresults))
+	if(num<=1000){
+		tt <- getForm(url, .params = args, curl = curl)
+		outlist <- xmlParse(tt)
+	} else
+		{
+			from <- seq(from=0, to=num, by=1000)
+			doit <- function(from) {
+				args$maxresults <- 1000
+				args$startindex <- from
+				tt <- getForm(url, .params = args, curl = curl)
+				xmlParse(tt)
+			}
+			outlist <- llply(from, doit, .parallel=parallel)
+		}
+	parseresults <- function(x) {
+		df <- gbifxmlToDataFrame(x, format)
 		df[, "decimalLongitude"] <- as.numeric(df[, "decimalLongitude"])
 		df[, "decimalLatitude"] <- as.numeric(df[, "decimalLatitude"])
 		i <- df[, "decimalLongitude"] == 0 & df[, "decimalLatitude"] == 0
 		if (removeZeros) {
 			df <- df[!i, ]
 		} else 
-			{
-				df[i, "decimalLatitude"] <- NA
-				df[i, "decimalLongitude"] <- NA 
-			}
+		{
+			df[i, "decimalLatitude"] <- NA
+			df[i, "decimalLongitude"] <- NA 
+		}
 		if(!is.null(writecsv)){
 			write.csv(df, file=writecsv)
 			message("Success! CSV file written")
 		}
-		df
-	} else { out }
+		df		
+	}
+	if(length(outlist)==1){parseresults(outlist)} else 
+		{
+			outt <- llply(outlist, parseresults, .parallel=parallel)
+			do.call(rbind, outt)	
+		}
 }
