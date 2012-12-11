bce7a2b361f09cc643bb52ef4558ec9ec0453067
Karthik Ram <karthik.ram@gmail.com>
2012-12-10 19:20:07 -0800

    updated error message.

--- R/occurrencedensity.r
+++ R/occurrencedensity.r
@@ -1,8 +1,8 @@
 #' Returns summary counts of occurrence records by one-degree cell for a single
 #' 		taxon, country, dataset, data publisher or data network.
-#' 
+#'
 #' @export
-occurrencedensity <- function() 
+occurrencedensity <- function()
 {
-	message("deprecated - now densitylist")
+	message("Function deprecated. Now use densitylist()")
 }
