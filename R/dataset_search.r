e1e262d7110df88cf8d144452ffdca4cbeb27187
schamberlain <myrmecocystus@gmail.com>
2013-09-04 10:03:14 -0700

    changes to dataset_search

--- R/dataset_search.r
+++ R/dataset_search.r
@@ -5,7 +5,7 @@
 #' 
 #' @import httr
 #' @importFrom plyr compact
-#' @param query
+#' @template occsearch
 #' @param type 
 #' @param keyword 
 #' @param owningOrg
@@ -13,7 +13,6 @@
 #' @param hostingOrg
 #' @param decade
 #' @param country
-#' @param callopts Further args passed on to GET.
 #' @export
 #' @examples \dontrun{
 #' # Gets all datasets of type "OCCURRENCE".
@@ -29,15 +28,70 @@
 #' # Fulltext search for all datasets having the word "amsterdam" somewhere in 
 #' # its metadata (title, description, etc).
 #' dataset_search(query="amsterdam")
+#' 
+#' # Limited search
+#' dataset_search(type="OCCURRENCE", limit=2)
+#' dataset_search(type="OCCURRENCE", limit=2, start=10)
+#' 
+#' # Return just descriptions
+#' dataset_search(type="OCCURRENCE", description=TRUE)
+#' 
+#' # Return metadata in a more human readable way (hard to manipulate though)
+#' dataset_search(type="OCCURRENCE", pretty=TRUE)
 #' }
-dataset_search <- function(query= NULL, type = NULL, keyword = NULL, 
+dataset_search <- function(query= NULL, type = NULL, keyword = NULL,
   owningOrg = NULL, networkOrigin = NULL, hostingOrg = NULL, decade = NULL, 
-  country = NULL, callopts=list())
+  country = NULL, limit=20, start=NULL, callopts=list(), pretty=FALSE, 
+  description=FALSE)
 {
   url <- 'http://api.gbif.org/dataset/search'
   args <- compact(list(q=query,type=type,keyword=keyword,owningOrg=owningOrg,
                        networkOrigin=networkOrigin,hostingOrg=hostingOrg,
-                       decade=decade,country=country))
+                       decade=decade,country=country,limit=limit,offset=start))
   tt <- content(GET(url, query=args, callopts))
-  tt
+  meta <- tt[!names(tt) == 'results']
+  tt$results[[1]]
+  
+  parse_dataset <- function(x){
+    data.frame(title=x$title,hostingOrganization=x$hostingOrganizationTitle,
+               owningOrganization=x$owningOrganizationTitle,
+               type=x$type,publishingCountry=x$publishingCountry,key=x$key,
+               hostingOrganizationKey=x$hostingOrganizationKey,
+               owningOrganizationKey=x$owningOrganizationKey)
+  }
+  
+  if(description){
+    out <- lapply(tt$results, "[[", "description")
+    names(out) <- sapply(tt$results, "[[", "title")
+  } else
+  {
+    if(length(tt$results)==1){
+      out <- parse_dataset(x=tt$results)
+    } else
+    {
+      out <- do.call(rbind, lapply(tt$results, parse_dataset))
+    }
+  }
+  
+  if(pretty){
+    printdata <- function(x){    
+      cat(paste("title:", x$title),
+          paste("hostingOrganization:", x$hostingOrganizationTitle),
+          paste("owningOrganization:", x$owningOrganizationTitle),
+          paste("type:", x$type),
+          paste("publishingCountry:", x$publishingCountry),
+          paste("hostingOrganizationKey:", x$hostingOrganizationKey),
+          paste("owningOrganizationKey:", x$owningOrganizationKey), 
+          paste("description:", x$description), "\n", sep="\n")
+    }
+    if(length(tt$results)==1){
+      printdata(tt$results)
+    } else
+    {
+      lapply(tt$results, printdata)[[1]]
+    }
+  } else
+  {
+    return( out )
+  }
 }
