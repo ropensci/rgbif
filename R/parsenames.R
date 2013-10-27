5452e4bb82f5754d4de8f4167953c3a4e86ae303
schamberlain <myrmecocystus@gmail.com>
2013-10-26 21:55:15 -0700

    added John as autho to parser fxn, added stop_for_status to parser fxn to check http status codes

--- R/parsenames.R
+++ R/parsenames.R
@@ -8,18 +8,21 @@
 #' @return A \code{data.frame} containing fields extracted from parsed 
 #' taxon names. Fields returned are the union of fields extracted from
 #' all species names in \code{scientificname}.
+#' @author John Baumgartner (johnbb@@student.unimelb.edu.au)
 #' @export
 #' @examples \dontrun{
-#' parsenames('x Agropogon littoralis')
+#' parsenames(scientificname='x Agropogon littoralis')
 #' parsenames(c('Arrhenatherum elatius var. elatius', 
 #'              'Secale cereale subsp. cereale', 'Secale cereale ssp. cereale',
 #'              'Vanessa atalanta (Linnaeus, 1758)'))
 #' }
 parsenames <- function(scientificname) {
   u <- "http://apidev.gbif.org/parser/name"
-  res <- content(POST('http://apidev.gbif.org/parser/name',
+  tt <- POST('http://apidev.gbif.org/parser/name',
                       config=c(add_headers('Content-Type' = 
                                              'application/json')),
-                      body=RJSONIO:::toJSON(scientificname)))
+                      body=RJSONIO:::toJSON(scientificname))
+  stop_for_status(tt)
+  res <- content(tt)
   do.call(rbind.fill, lapply(res, as.data.frame))
 }
