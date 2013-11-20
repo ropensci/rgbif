543dcd72c88b9efe6df4b90e4df18be8b68655d6
schamberlain <myrmecocystus@gmail.com>
2013-11-19 16:13:54 -0800

    added some missing files lost in merge, and deleted some

--- R/gbifmap.r
+++ R/gbifmap.r
@@ -1,81 +1,46 @@
-#' Make a simple map to visualize GBIF point data.
+#' Make a map to visualize GBIF occurrence data.
 #' 
 #' @template map
 #' @import grid ggplot2 maps
 #' @examples \dontrun{
-#' gbifmap_list(input = out) # make a map using vertmap
-#' 
-#' # Point map, using output from occurrencelist, example 2, a species with more data
-#' out <- occurrencelist(scientificname = 'Puma concolor', coordinatestatus = TRUE, 
-#'    maxresults = 100)
-#' gbifmap_list(input = out) # make a map
-#' gbifmap_list(input = out, region = 'USA') # make a map, just using the US map
-#' 
-#' # Point map, using output from occurrencelist, many species
-#' splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa')
-#' out <- occurrencelist_many(splist, coordinatestatus = TRUE, maxresults = 20)
-#' gbifmap_list(out)
-#' 
-#' # Point map, using output from occurrencelist, many species
-#' splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa', 'Ceyx fallax', 
-#'    'Picoides lignarius', 'Campephilus leucopogon')
-#' out <- occurrencelist_many(splist, coordinatestatus = TRUE, maxresults = 100)
-#' gbifmap_list(out)
-#' 
-#' # Get occurrences or density by area, using min/max lat/long coordinates
-#' # Setting scientificname="*" so we just get any species
-#' out <- occurrencelist(scientificname="*", minlatitude=30, maxlatitude=35,
-#'    minlongitude=-100, maxlongitude=-95, coordinatestatus = TRUE, maxresults = 500)
-#' 
-#' # Using `geom_point`
-#' gbifmap_list(out, "state", "texas", geom_point)
-#' 
-#' # Using geom_jitter to move the points apart from one another
-#' gbifmap_list(out, "state", "texas", geom_jitter, position_jitter(width = 0.3, 
-#'    height = 0.3))
-#' 
-#' # And move points a lot
-#' gbifmap_list(out, "state", "texas", geom_jitter, position_jitter(width = 1, height = 1))
-#' 
-#' # Customize the plot by passing options to `ggplot()`
-#' mycustom <- function(){
-#'    list(geom_point(size=9)
-#'        )}
-#' out <- occurrencelist(scientificname = 'Accipiter erythronemius', 
-#'    coordinatestatus = TRUE, maxresults = 100)
-#' gbifmap_list(out, customize = mycustom())
+#' # Make a map of Puma concolor occurrences
+#' key <- gbif_lookup(name='Puma concolor', kingdom='plants')$speciesKey
+#' dat <- occ_search(taxonKey=key, return='data', limit=300)
+#' gbifmap(input=dat)
+#'
+#' # Plot more Puma concolor occurrences
+#' dat <- occ_search(taxonKey=key, return='data', limit=1200)
+#' nrow(dat)
+#' gbifmap(input=dat)
 #' }
 #' @export
-gbifmap_list <- function(input = NULL, mapdatabase = "world", region = ".", 
-                     geom = geom_point, jitter = NULL, customize = NULL)
+gbifmap <- function(input = NULL, mapdatabase = "world", region = ".", 
+                    geom = geom_point, jitter = NULL, customize = NULL)
 {
   long = NULL
   lat = NULL
   group = NULL
-  decimalLongitude = NULL
-  decimalLatitude = NULL
-  taxonName = NULL
+  longitude = NULL
+  latitude = NULL
+  name = NULL
   
-  if(!is.gbiflist(input))
-    stop("Input is not of class gbiflist")
+#   if(!is.gbiflist(input))
+#     stop("Input is not of class gbiflist")
+
+  tomap <- input[complete.cases(input$latitude, input$latitude), ]
+  tomap <- tomap[!tomap$longitude==0 & !tomap$latitude==0,]
+  tomap <- input[-(which(tomap$latitude <=90 || tomap$longitude <=180)), ]
+  tomap$name <- as.factor(gbif_capwords(tomap$name, onlyfirst=TRUE))
   
-  input <- data.frame(input)
-  input$decimalLatitude <- as.numeric(input$decimalLatitude)
-  input$decimalLongitude <- as.numeric(input$decimalLongitude)
-  
-  tomap <- input[complete.cases(input$decimalLatitude, input$decimalLatitude), ]
-  tomap <- input[-(which(tomap$decimalLatitude <=90 || tomap$decimalLongitude <=180)), ]
-  tomap$taxonName <- as.factor(capwords(tomap$taxonName, onlyfirst=TRUE))
-  
-  if(length(unique(tomap$taxonName))==1){ theme2 <- theme(legend.position="none") } else 
+  if(length(unique(tomap$name))==1){ theme2 <- theme(legend.position="none") } else 
   { theme2 <- NULL }
   
-  world <- map_data(map=mapdatabase, region=region) # get world map data
+  world <- map_data(map=mapdatabase, region=region)
   message(paste("Rendering map...plotting ", nrow(tomap), " points", sep=""))
   
-  ggplot(world, aes(long, lat)) + # make the plot
+  ggplot(world, aes(long, lat)) +
     geom_polygon(aes(group=group), fill="white", color="gray40", size=0.2) +
-    geom(data=tomap, aes(decimalLongitude, decimalLatitude, colour=taxonName), 
+    geom(data=tomap, aes(longitude, latitude, colour=name), 
          alpha=0.4, size=3, position=jitter) +
     scale_color_brewer("", type="qual", palette=6) +
     labs(x="", y="") +
@@ -85,167 +50,4 @@ gbifmap_list <- function(input = NULL, mapdatabase = "world", region = ".",
     blanktheme() +
     theme2 + 
     customize
-}
-
-
-#' Make a simple map to visualize GBIF data density data
-#' 
-#' @template map
-#' @examples \dontrun{
-#' # Tile map, using output from densitylist, Canada
-#' out2 <- densitylist(originisocountrycode = "CA") # data for Canada
-#' gbifmap_dens(out2) # on world map
-#' gbifmap_dens(out2, region="Canada") # on Canada map
-#' 
-#' # Tile map, using gbifdensity, a specific data provider key
-#' # 191 for 'University of Texas at El Paso'
-#' out2 <- densitylist(dataproviderkey = 191) # data for the US
-#' gbifmap_dens(out2) # on world map
-#' 
-#' # Modify the plotting region
-#' out <- densitylist(originisocountrycode="US")
-#' gbifmap_dens(out, mapdatabase="usa")
-#' }
-#' @export
-gbifmap_dens <- function(input = NULL, mapdatabase = "world", region = ".", 
-                             geom = geom_point, jitter = NULL, customize = NULL)
-{
-  long = NULL
-  lat = NULL
-  group = NULL
-  
-  if(!is.gbifdens(input))
-    stop("Input is not of class gbifdens")
-  
-  input <- data.frame(input)
-  middf <- data.frame(
-    lat = input$minLatitude+0.5,
-    long = input$minLongitude+0.5,
-    count = input$count
-  )
-  mapp <- map_data(map=mapdatabase, region=region)
-  message(paste("Rendering map...plotting ", nrow(input), " tiles", sep=""))
-  ggplot(mapp, aes(long, lat)) + # make the plot
-    geom_raster(data=middf, aes(long, lat, fill=log10(count), width=1, height=1)) +
-    scale_fill_gradient2(low = "white", mid="blue", high = "black") +
-    geom_polygon(aes(group=group), fill="white", alpha=0, color="gray80", size=0.8) +
-    labs(x="", y="") +
-    theme_bw(base_size=14) + 
-    theme(legend.position = "bottom", legend.key = element_blank()) +
-    blanktheme() +
-    customize
-}
-
-#' Make a simple map to visualize GBIF data.
-#' 
-#' Basic function to plot your lat/long data on a map.
-#' 
-#' @import ggplot2 maps
-#' @param input Either a single data.frame or a list of data.frame's (e.g., from
-#'    different speies). The data.frame has to have, in addition to any other 
-#'    columns, columns named exactly "decimalLatitude" and "decimalLongitude".
-#' @param mapdatabase The map database to use in mapping. What you choose here 
-#'    determines what you can choose in the region parameter. One of: county, 
-#'    state, usa, world, world2, france, italy, or nz. 
-#' @param region The region of the world to map. From the maps package, run 
-#'    \code{sort(unique(map_data("world")$region))} to see region names for the
-#'    world database layer, or e.g., \code{sort(unique(map_data("state")$region))}
-#'    for the state layer.
-#' @param geom The geom to use, one of geom_point or geom_jitter. Don't 
-#'   	quote them. 
-#' @param jitter If you use jitterposition, the amount by which to jitter 
-#' 		points in width, height, or both. 
-#' @param customize Further arguments passed on to ggplot. 
-#' @return Map (using ggplot2 package) of points or tiles on a world map.
-#' @details gbifmap takes care of cleaning up the data.frame (removing NA's, etc.) 
-#'    returned from rgbif functions, and creating the map. This function
-#'    gives a simple map of your data.  You can look at the code behing the 
-#'    function itself if you want to build on it to make a map according 
-#'    to your specfications.
-#' @examples \dontrun{
-#' # Point map, using output from occurrencelist, example 1
-#' out <- occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100)
-#' gbifmap(input = out) # make a map using vertmap
-#' 
-#' # Point map, using output from occurrencelist, example 2, a species with more data
-#' out <- occurrencelist(scientificname = 'Puma concolor', coordinatestatus = TRUE, maxresults = 100)
-#' gbifmap(input = out) # make a map
-#' gbifmap(input = out, region = 'USA') # make a map, just plotting data for 
-#' 
-#' # Point map, using output from occurrencelist, many species
-#' splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa')
-#' out <- llply(splist, function(x) occurrencelist(x, coordinatestatus = T, maxresults = 100))
-#' gbifmap(out)
-#' 
-#' # Tile map, using output from densitylist, Canada
-#' out2 <- densitylist(originisocountrycode = "CA") # data for Canada
-#' gbifmap(out2) # on world map
-#' gbifmap(out2, region="Canada") # on Canada map
-#' 
-#' # Tile map, using gbifdensity, a specific data provider key
-#' # 191 for 'University of Texas at El Paso'
-#' out2 <- densitylist(dataproviderkey = 191) # data for the US
-#' gbifmap(out2) # on world map
-#' 
-#' # Get occurrences or density by area, using min/max lat/long coordinates
-#' out <- occurrencelist(minlatitude=30, maxlatitude=35, minlongitude=-100, maxlongitude=-95, 
-#'    coordinatestatus = T, maxresults = 5000, latlongdf = T)
-#' 
-#' # Using `geom_point`
-#' gbifmap(out, "state", "texas", geom_point)
-#' 
-#' # Using geom_jitter to move the points apart from one another
-#' gbifmap(out, "state", "texas", geom_jitter, position_jitter(width = 0.3, height = 0.3))
-#' 
-#' # And move points a lot
-#' gbifmap(out, "state", "texas", geom_jitter, position_jitter(width = 1, height = 1))
-#' 
-#' # Customize the plot by passing options to `ggplot()`
-#' mycustom <- function(){
-#'    list(geom_point(size=9)
-#'        )}
-#' out <- occurrencelist(scientificname = 'Accipiter erythronemius', coordinatestatus = TRUE, maxresults = 100)
-#' gbifmap(out, customize = mycustom())
-#' }
-#' @export
-gbifmap <- function(input = NULL, mapdatabase = "world", region = ".", 
-                    geom = geom_point, jitter = NULL, customize = NULL)
-{
-  stop("This function is deprecated. See ?gbifmap_list and ?gbifmap_dens")
-
-  long <- lat <- group <- decimalLongitude <- decimalLatitude <- taxonName <- NULL
-  
-  if(is(input, "list"))
-    input <- ldply(input, data.frame)
-
-  if(all(names(input) %in% c("cellid","minLatitude","maxLatitude","minLongitude","maxLongitude","count"))==TRUE){
-    middf <- data.frame(
-      lat = input$minLatitude+0.5,
-      long = input$minLongitude+0.5,
-      count = input$count
-    )
-    mapp <- map_data(map=mapdatabase, region=region)
-    message(paste("Rendering map...plotting ", nrow(input), " tiles", sep=""))
-    ggplot(mapp, aes(long, lat)) + # make the plot
-      geom_raster(data=middf, aes(long, lat, fill=log10(count), width=1, height=1)) +
-      scale_fill_gradient2(low = "white", mid="blue", high = "black") +
-      geom_polygon(aes(group=group), fill="white", alpha=0, color="gray80", size=0.8) +
-      labs(x="", y="") +
-      theme_bw(base_size=14) + 
-      customize
-  } else
-  {
-    tomap <- input[complete.cases(input$decimalLatitude, input$decimalLatitude), ]
-    tomap <- input[-(which(tomap$decimalLatitude <=90 || tomap$decimalLongitude <=180)), ]
-    world <- map_data(map=mapdatabase, region=region) # get world map data
-    message(paste("Rendering map...plotting ", nrow(tomap), " points", sep=""))
-    tomap$taxonName <- as.factor(capwords(tomap$taxonName, onlyfirst=T))
-    ggplot(world, aes(long, lat)) + # make the plot
-      geom_polygon(aes(group=group), fill="white", color="gray40", size=0.2) +
-      geom(data=tomap, aes(decimalLongitude, decimalLatitude, colour=taxonName), 
-           alpha=0.4, size=3, position=jitter) +
-      labs(x="", y="") +
-      theme_bw(base_size=14) + 
-      customize
-  }
 }
