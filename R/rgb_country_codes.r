1129c7c56971abeaefb84f1b2edbd4ea73a3372a
schamberlain <myrmecocystus@gmail.com>
2013-12-14 10:12:58 -0500

    woops, now with rgb_country codes

---
+++ R/rgb_country_codes.r
@@ -0,0 +1,17 @@
+#' Look up 2 character ISO country codes
+#' 
+#' @param country_name Name of country to look up
+#' @param fuzzy If TRUE, uses agrep to do fuzzy search on names.
+#' @param ... Further arguments passed on to agrep or grep
+#' @export
+#' @examples
+#' rgb_country_codes(country_name="United")
+rgb_country_codes <- function(country_name, fuzzy=FALSE, ...)
+{
+  if(fuzzy){
+    isocodes[agrep(country_name, isocodes$name, ...),]
+  } else
+  {
+    isocodes[grep(country_name, isocodes$name, ...),]
+  }
+}
