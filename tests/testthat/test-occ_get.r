b714664e852a9854df25aac6ebcc98bfb3507160
Scott Chamberlain <myrmecocystus@gmail.com>
2014-04-23 10:45:54 -0700

    added tests for verbatim data in occ_get

--- tests/testthat/test-occ_get.r
+++ tests/testthat/test-occ_get.r
@@ -4,6 +4,11 @@ tt <- occ_get(key=766766824, return='data')
 uu <- occ_get(key=766766824, 'hier')
 vv <- occ_get(key=766766824, 'all')
 
+aa <- occ_get(key=766766824, verbatim = TRUE)
+bb <- occ_get(key=766766824, verbatim = TRUE, fields = "all")
+cc <- occ_get(key=c(766766824,620594291,766420684), 
+        fields=c('scientificName','decimalLatitude','basisOfRecord'), verbatim=TRUE)
+
 test_that("returns the correct class", {
   expect_is(tt, "data.frame")
   expect_is(tt[1,1], "character")
@@ -16,6 +21,14 @@ test_that("returns the correct class", {
   expect_is(vv, "list")
   expect_is(vv[[1]], "data.frame")
   expect_is(vv[[2]], "data.frame")
+  
+  expect_is(aa, "data.frame")
+  expect_is(aa[1,1], "character")
+  expect_is(aa[1,2], "numeric")
+  
+  expect_is(bb, "data.frame")
+  expect_is(bb[1,10], "character")
+  expect_is(bb[1,2], "character")
 })
 
 test_that("returns the correct dimensions", {
@@ -24,4 +37,8 @@ test_that("returns the correct dimensions", {
   expect_equal(dim(vv), NULL)
   expect_equal(dim(vv[[1]]), c(7,3))
   expect_equal(dim(vv[[2]]), c(1,4))
+  
+  expect_equal(dim(aa), c(1,4))
+  expect_equal(dim(bb), c(1,34))
+  expect_equal(dim(cc), c(3,3))
 })
