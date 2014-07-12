c6e78fbac04375c9ca1102787249bee8e3644cf1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-12 08:25:56 -0700

    added more country egs, fixed broken dataset metrics test

--- tests/testthat/test-dataset_metrics.r
+++ tests/testthat/test-dataset_metrics.r
@@ -9,6 +9,6 @@ test_that("returns the correct class", {
 })
 
 test_that("returns the correct value", {
-  expect_equal(tt$key, 1691)
+  expect_equal(tt$key, 4341)
   expect_equal(tt$datasetKey, "3f8a1297-3259-4700-91fc-acc4170b27ce")
 })
