3d15466728ea0da35ab56f23775db646b05f498e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-15 08:40:45 -0400

    fixed tutorial url in startup message, closes #105

--- R/startup.r
+++ R/startup.r
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-  packageStartupMessage("\nNew to rgbif? Tutorial at http://ropensci.org/tutorials/rgbif_tutorial_newapi.html \nSee http://ropensci.org/tutorials/rgbif_tutorial.html for the old GBIF API \ncitation(package='rgbif') for the citation for this package \nUse suppressPackageStartupMessages() to suppress these startup messages in the future")
+  packageStartupMessage("\nNew to rgbif? Tutorial at http://ropensci.org/tutorials/rgbif_tutorial.html \ncitation(package='rgbif') for the citation for this package \nUse suppressPackageStartupMessages() to suppress these startup messages in the future")
 } 
