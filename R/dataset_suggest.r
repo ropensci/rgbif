4b78ed7ef13bdcb9c1b88e71b5a5350c2bbe704a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-07 13:29:49 -0700

    fixed up dataset_suggest, some bugs in outputs, and added in publishingOrg param

--- R/dataset_suggest.r
+++ R/dataset_suggest.r
@@ -22,11 +22,11 @@
 #' 
 #' # Suggest datasets owned by the organization with key 
 #' # "07f617d0-c688-11d8-bf62-b8a03c50a862" (UK NBN).
-#' dataset_suggest(owningOrg="07f617d0-c688-11d8-bf62-b8a03c50a862")
+#' head(dataset_suggest(owningOrg="07f617d0-c688-11d8-bf62-b8a03c50a862"))
 #' 
 #' # Fulltext search for all datasets having the word "amsterdam" somewhere in 
 #' # its metadata (title, description, etc).
-#' dataset_suggest(query="amsterdam")
+#' head(dataset_suggest(query="amsterdam"))
 #' 
 #' # Limited search
 #' dataset_suggest(type="OCCURRENCE", limit=2)
@@ -40,72 +40,65 @@
 #' 
 #' # Search by country code. Lookup isocodes first, and use US for United States
 #' isocodes[agrep("UNITED", isocodes$gbif_name),]
-#' dataset_suggest(country="US")
+#' head(dataset_suggest(country="US"))
 #' 
 #' # Search by decade
-#' dataset_suggest(decade=1980)
+#' head(dataset_suggest(decade=1980))
 #' }
 
 dataset_suggest <- function(query = NULL, country = NULL, type = NULL, subtype = NULL, 
-  keyword = NULL, owningOrg = NULL, hostingOrg = NULL, publishingCountry = NULL, 
-  decade = NULL, continent = NULL, limit=100, start=NULL, callopts=list(), 
+  keyword = NULL, owningOrg = NULL, publishingOrg = NULL, hostingOrg = NULL, 
+  publishingCountry = NULL, decade = NULL, continent = NULL, limit=100, start=NULL, callopts=list(), 
   pretty=FALSE, description=FALSE)
 {
   url <- paste0(gbif_base(), '/dataset/suggest')
   args <- rgbif_compact(list(q=query,type=type,keyword=keyword,owningOrg=owningOrg,
+                       publishingOrg=publishingOrg, 
                        hostingOrg=hostingOrg,publishingCountry=publishingCountry,
                        decade=decade,limit=limit,offset=start))
   tt <- gbif_GET(url, args, callopts)
   
-  parse_dataset <- function(x){
-    tmp <- rgbif_compact(list(
-      key=x$key,
-      type=x$type,
-      title=x$title,
-      hostingOrganization=x$hostingOrganizationTitle,
-      hostingOrganizationKey=x$hostingOrganizationKey,
-      owningOrganization=x$owningOrganizationTitle,
-      owningOrganizationKey=x$owningOrganizationKey,
-      publishingCountry=x$publishingCountry
-    ))
-    data.frame(tmp, stringsAsFactors=FALSE)
-  }
-  
   if(description){
-    out <- vapply(tt, "[[", "", "title")
-#     names(out) <- sapply(tt, "[[", "title")
+    out <- sapply(tt, "[[", "description")
+    names(out) <- sapply(tt, "[[", "title")
+    out <- rgbif_compact(out)
   } else
   {
     if(length(tt)==1){
-      out <- parse_dataset(x=tt$results)
+      out <- parse_suggest(x=tt$results)
     } else
     {
-      out <- do.call(rbind.fill, lapply(tt, parse_dataset))
+      out <- do.call(rbind.fill, lapply(tt, parse_suggest))
     }
   }
   
   if(pretty){
-    printdata <- function(x){    
-      cat(
-        paste("type:", x$key),
-        paste("type:", x$type),
-        paste("title:", x$title),
-        paste("hostingOrganization:", x$hostingOrganizationTitle),
-        paste("hostingOrganizationKey:", x$hostingOrganizationKey),
-        paste("owningOrganization:", x$owningOrganizationTitle),
-        paste("owningOrganizationKey:", x$owningOrganizationKey), 
-        paste("publishingCountry:", x$publishingCountry),
-        paste("description:", x$description
-        ), "\n", sep="\n")
-    }
     if(length(tt)==1){
-      invisible(printdata(tt))
+      invisible(print_suggest(tt))
     } else
     {
-      invisible(lapply(tt, printdata)[[1]])
+      invisible(lapply(tt, print_suggest)[[1]])
     }
   } else
   {
     return( out )
   }
+}
+
+parse_suggest <- function(x){
+  tmp <- rgbif_compact(list(
+    key=x$key,
+    type=x$type,
+    title=x$title
+  ))
+  data.frame(tmp, stringsAsFactors=FALSE)
+}
+
+print_suggest <- function(x){    
+  cat(
+    paste("datasetKey:", x$key),
+    paste("type:", x$type),
+    paste("datasetTitle:", x$title),
+    paste("description:", x$description), 
+    "\n", sep="\n")
 }
