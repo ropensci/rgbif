864b4150d4cae279be2b7e41de9eef32882e16f7
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-14 23:14:10 -0700

    occ_issues parses issues in place now, closes #122

--- R/occ_issues.r
+++ R/occ_issues.r
@@ -1,6 +1,7 @@
 #' Parse and examine further GBIF issues on a dataset.
 #'
 #' @export
+#' 
 #' @param .data Output from a call to \code{occ_search}
 #' @param ... Named parameters to only get back (e.g., cdround), or to remove (e.g. -cdround).
 #' @param mutate (character) One of: 
@@ -11,26 +12,53 @@
 #' }
 #' For split and split_expand, values in cells become y ("yes") or n ("no").
 #' 
+#' @details See also the vignette \code{Issue handling}. 
+#' 
+#' Note that you can also query pased on issues, e.g., 
+#' \code{occ_search(taxonKey=1, issue='DEPTH_UNLIKELY')}. However, I imagine it's more likely
+#' that you want to search for occurrences based on a taxonomic name, or geographic area,
+#' not based on issues, so it makes sense to pull data down, then clean as needed 
+#' using this function.
+#' 
+#' This function only affects the \code{data} element in the \code{gbif} class that is 
+#' returned from a call to \code{\link[rgbif]{occ_search}}. Maybe in a future version
+#' we will remove the associated records from the \code{hierarchy} and \code{media}
+#' elements as they are remove from the \code{data} element. 
+#' 
 #' @examples \donttest{
 #' # Parsing output by issue
 #' (res <- occ_search(geometry='POLYGON((30.1 10.1, 10 20, 20 40, 40 40, 30.1 10.1))', limit = 50))
+#' 
 #' ## what do issues mean, can print whole table, or search for matches
 #' head(gbifissues)
 #' gbifissues[ gbifissues$code %in% c('cdround','cudc','gass84','txmathi'), ]
+#' 
 #' ## or parse issues in various ways
+#' ### inlude only rows with gass84 issue 
+#' gg <- res %>% occ_issues(gass84)
+#' NROW(res$data)
+#' NROW(gg$data)
+#' head(res$data)[,c(1:5)]
+#' head(gg$data)[,c(1:5)]
+#' 
 #' ### remove data rows with certain issue classes
-#' library('magrittr')
 #' res %>% occ_issues(-gass84, -mdatunl)
+#' 
 #' ### split issues into separate columns
 #' res %>% occ_issues(mutate = "split")
+#' res %>% occ_issues(-gass84, -mdatunl, mutate = "split")
+#' res %>% occ_issues(gass84, mutate = "split")
+#' 
 #' ### expand issues to more descriptive names
 #' res %>% occ_issues(mutate = "expand")
+#' 
 #' ### split and expand
 #' res %>% occ_issues(mutate = "split_expand")
+#' 
 #' ### split, expand, and remove an issue class
 #' res %>% occ_issues(-gass84, mutate = "split_expand")
 #' 
-#' # Or you can use occ_issues without %>%
+#' ## Or you can use occ_issues without %>%
 #' occ_issues(res, -gass84, mutate = "split_expand")
 #' }
 
@@ -38,7 +66,7 @@ occ_issues <- function(.data, ..., mutate=NULL){
   assert_that(is(.data, "gbif"))
   tmp <- .data$data
   
-  if(!is.null(filter)){
+  if(!length(dots(...)) == 0){
     filters <- parse_input(...)
     if(length(filters$neg) > 0){
       tmp <- tmp[ !grepl(paste(filters$neg, collapse = "|"), tmp$issues), ]
@@ -63,7 +91,6 @@ occ_issues <- function(.data, ..., mutate=NULL){
   return( .data )
 }
 
-
 mutate_iss <- function(w){
   w$issues <- sapply(strsplit(w$issues, split=","), function(z) paste(gbifissues[ gbifissues$code %in% z, "issue" ], collapse = ",") )
   return( w )
@@ -74,7 +101,9 @@ split_iss <- function(m){
   df <- data.frame(rbindlist(lapply(strsplit(m$issues, split=","), function(b) { v <- unq %in% b; data.frame(rbind(ifelse(v, "y", "n")), stringsAsFactors = FALSE) } )), stringsAsFactors = FALSE)
   names(df) <- unq
   m$issues <- NULL
-  m <- cbind(m, df)
+#   m <- cbind(m, df)
+  first <- c('name','key','decimalLatitude','decimalLongitude')
+  m <- data.frame(m[,first], df, m[,!names(m) %in% first], stringsAsFactors = FALSE)
   return( m )
 }
 
@@ -88,3 +117,11 @@ parse_input <- function(...){
 dots <- function(...){
   eval(substitute(alist(...)))
 }
+
+# shit <- function(...){
+#   if(length(dots(...)) == 0) "empty" else "not empty"
+# }
+# 
+# shit <- function(...){
+#   dots(...)
+# }
