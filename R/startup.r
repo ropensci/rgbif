6c087a43ed718b1cb7724c42d430f968382ef0a0
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-02 19:07:53 -0700

    removed startup message

--- R/startup.r
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("\nNew to rgbif? Tutorial at http://ropensci.org/tutorials/rgbif_tutorial.html \ncitation(package='rgbif') for the citation for this package \nUse suppressPackageStartupMessages() to suppress these startup messages in the future")
-} 
