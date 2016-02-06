094af81fdcda6934cffd784de32301462ba4c821
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-05 16:25:03 -0800

    clean up code a bit, move geometry handlers to sep file

---
+++ R/geometry_utils.R
@@ -0,0 +1,31 @@
+geometry_handler <- function(x) {
+  if (!is.null(x)) {
+    if (!is.character(x)) {
+      x <- gbif_bbox2wkt(bbox = x)
+    }
+    if (nchar(x) > 1500) {
+      message("geometry is big, querying BBOX, then pruning results to polygon")
+      # set run time option so that we know to prune result once it returns
+      options(rgbif.geometry.original = x)
+      x <- gbif_bbox2wkt(bbox = gbif_wkt2bbox(x))
+    }
+  }
+  return(x)
+}
+
+prune_result <- function(x) {
+  geom_orig <- getOption('rgbif.geometry.original')
+  if (!is.null(geom_orig)) {
+    check_for_a_pkg("sp")
+    check_for_a_pkg("rgeos")
+    poly_orig <- rgeos::readWKT(geom_orig)
+    xx <- x
+    sp::coordinates(xx) <- ~decimalLongitude + decimalLatitude
+    clipped <- sp::over(xx, poly_orig) == 1
+    clipped[is.na(clipped)] <- FALSE
+    x <- x[clipped, ]
+  }
+  # set rgbif.geometry.original to NULL on return
+  options(rgbif.geometry.original = NULL)
+  return(x)
+}
--- R/occ_search.r
+++ R/occ_search.r
@@ -166,46 +166,6 @@ occ_search <- function(taxonKey=NULL, scientificName=NULL, country=NULL, publish
   structure(out, return = return, args = argscoll)
 }
 
-# geometry_handler <- function(x) {
-#   if(!is.null(x)){
-#     if(!is.character(x)){
-#       gbif_bbox2wkt(bbox=x)
-#     } else { x }
-#   } else { x }
-# }
-
-geometry_handler <- function(x) {
-  if (!is.null(x)) {
-    if (!is.character(x)) {
-      x <- gbif_bbox2wkt(bbox = x)
-    }
-    if (nchar(x) > 1500) {
-      message("geometry is big, querying BBOX, then pruning results to polygon")
-      # set run time option so that we know to prune result once it returns
-      options(rgbif.geometry.original = x)
-      x <- gbif_bbox2wkt(bbox = gbif_wkt2bbox(x))
-    }
-  }
-  return(x)
-}
-
-prune_result <- function(x) {
-  geom_orig <- getOption('rgbif.geometry.original')
-  if (!is.null(geom_orig)) {
-    check_for_a_pkg("sp")
-    check_for_a_pkg("rgeos")
-    poly_orig <- rgeos::readWKT(geom_orig)
-    xx <- x
-    coordinates(xx)<-~decimalLongitude+decimalLatitude
-    clipped <- sp::over(xx, poly_orig) == 1
-    clipped[is.na(clipped)] <- FALSE
-    x <- x[clipped, ]
-  }
-  # set option to NULL on return
-  options(rgbif.geometry.original = NULL)
-  return(x)
-}
-
 #' @method print gbif
 #' @export
 #' @rdname occ_search
