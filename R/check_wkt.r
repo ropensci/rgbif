cf0977b0956c97b363b5991291b6a4a46b1e56ea
Scott Chamberlain <myrmecocystus@gmail.com>
2017-06-14 09:33:00 -0700

    dontrun examples for check_wkt for now, until problem with validate wicket fixed

--- R/check_wkt.r
+++ R/check_wkt.r
@@ -2,13 +2,13 @@
 #'
 #' @export
 #' @param wkt (character) one or more Well Known Text objects
-#' @examples
+#' @examples \dontrun{
 #' check_wkt('POLYGON((30.1 10.1, 10 20, 20 60, 60 60, 30.1 10.1))')
 #' check_wkt('POINT(30.1 10.1)')
 #' check_wkt('LINESTRING(3 4,10 50,20 25)')
 #'
-#' wkt <- 'MULTIPOLYGON(((30 20, 45 40, 10 40, 30 20)),
-#' ((15 5, 40 10, 10 20, 5 10, 15 5)))'
+#' wkt <- 'POLYGON((30 20, 10 40, 45 40, 30 20),
+#' (15 5, 5 10, 10 20, 40 10, 15 5))'
 #' check_wkt(gsub("\n", '', wkt))
 #'
 #' # check many passed in at once
@@ -42,6 +42,7 @@
 #' wkt <- "POLYGON((-102.2 46.0,-93.9 46.0,-93.9 43.7,-102.2 43.7,-102.2 46.0))
 #' ;POLYGON((30.1 10.1, 10 20, 20 40, 40 40, 30.1 10.1))"
 #' check_wkt(gsub("\n", '', wkt))
+#' }
 
 check_wkt <- function(wkt = NULL){
   if (!is.null(wkt)) {
