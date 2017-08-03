3f297f8ee1b1f0e7d59b3915c7d6bd5b6f9993f0
Scott Chamberlain <myrmecocystus@gmail.com>
2017-08-03 15:57:18 -0700

    #266 download queue fxn, bump version

---
+++ R/download_queue.R
@@ -0,0 +1,133 @@
+#' GBIF download queue
+#'
+#' @export
+#' @keywords internal
+#' @details
+#' **Methods**
+#'   \describe{
+#'     \item{`add(x)`}{
+#'       Add single jobs to the queue
+#'     }
+#'     \item{`add_all()`}{
+#'       Add all jobs to the queue
+#'     }
+#'     \item{`remove(x)`}{
+#'       Remove a job from the queue
+#'     }
+#'     \item{`jobs()`}{
+#'       Give number of jobs in the `queue`
+#'     }
+#'   }
+#' @format NULL
+#' @usage NULL
+#' @examples
+#' x <- GbifQueue$new(
+#'   occ_download('taxonKey = 3119195', "year = 1976"),
+#'   occ_download('taxonKey = 3119195', "year = 2001"),
+#'   occ_download('taxonKey = 3119195', "year = 2001", "month <= 8"),
+#'   occ_download('taxonKey = 3119195', "year = 2004"),
+#'   occ_download('taxonKey = 3119195', "year = 2005")
+#' )
+#' que = x
+#' x
+#' x$reqs
+#' x$add_all()
+#' x$jobs()
+#' x
+#' x$remove(x$reqs[[1]])
+#' x
+
+GbifQueue <- R6::R6Class(
+  'GbifQueue',
+  public = list(
+    reqs = NULL,
+
+    print = function(x, ...) {
+      cat("<GBIF download many queue> ", sep = "\n")
+      cat(paste0("  requests (N): ", length(self$reqs)), sep = "\n")
+      cat(paste0("  requests in queue (N): ", length(self$queue)), sep = "\n")
+      invisible(self)
+    },
+
+    initialize = function(...) {
+      self$reqs <- lapply(lazyeval::lazy_dots(...), DownReq$new)
+      self$reqs <- stats::setNames(self$reqs, seq_along(self$reqs))
+    },
+
+    add = function(x) {
+      self$queue <- c(self$queue,
+                         stats::setNames(list(x), digest::digest(x$req$expr)))
+    },
+
+    add_all = function() {
+      if (length(self$reqs) == 0) stop("no jobs to add")
+      invisible(lapply(self$reqs, function(z) self$add(z)))
+    },
+
+    remove = function(x) {
+      self$queue[digest::digest(x$req$expr)] <- NULL
+    },
+
+    queue = list(),
+    jobs = function() length(self$queue)
+  )
+)
+
+#' Download request
+#'
+#' @export
+#' @keywords internal
+#' @details
+#' **Methods**
+#'   \describe{
+#'     \item{`new(...)`}{
+#'       initialize request
+#'     }
+#'     \item{`run(...)`}{
+#'       execute http request
+#'     }
+#'     \item{`status(...)`}{
+#'       check http request status
+#'     }
+#'   }
+#'
+#' @format NULL
+#' @usage NULL
+#' @examples \dontrun{
+#' res <- DownReq$new(occ_download('taxonKey = 3119195', "year = 1991"))
+#' res
+#' res$req
+#' res$run()
+#' # (requests <- GbifQueue$new())
+#' # res$run(keep_track = TRUE)
+#' # requests
+#' res$status()
+#' }
+DownReq <- R6::R6Class(
+  'DownReq',
+  public = list(
+    req = NULL,
+    result = NULL,
+
+    initialize = function(x) {
+      self$req <- x
+    },
+
+    print = function(x, ...) {
+      cat("<GBIF download single queue> ", sep = "\n")
+      print(self$req$expr)
+      invisible(self)
+    },
+
+    run = function() {
+      tmp <- tryCatch(lazyeval::lazy_eval(self$req), error = function(e) e)
+      self$result <- if (inherits(tmp, "error")) NULL else tmp
+    },
+
+    status = function() {
+      if (is.null(self$result)) stop("run() result is `NULL`, not checking status")
+      tmp <- occ_download_meta(self$result)
+      return(tmp)
+    }
+  )
+)
---
+++ R/occ_download_queue.R
@@ -0,0 +1,104 @@
+#' Download requests in a queue
+#'
+#' @export
+#' @param ... any number of [occ_download()] requests
+#' @return a list of `occ_download` class objects, see [occ_download_get()]
+#' to fetch data
+#' @details This function is a convenience wrapper around [occ_download()],
+#' allowing the user to kick off any number of requests, while abiding by
+#' GBIF rules of 3 concurrent requests per user.
+#'
+#' @section How it works:
+#' It works by using lazy evaluation to collect your requests into a queue.
+#' Then it kicks of the first 3 requests. Then in a while loop, we check
+#' status of those requests, and when any request finishes, we kick off the
+#' next, and so on. So in theory, there may not always strictly be 3 running
+#' concurrently, but the function will usually provide for 3 running
+#' concurrently.
+#' @examples \dontrun{
+#' out <- occ_download_queue(
+#'   occ_download('taxonKey = 3119195', "year = 1976"),
+#'   occ_download('taxonKey = 3119195', "year = 2001"),
+#'   occ_download('taxonKey = 3119195', "year = 2001", "month <= 8"),
+#'   occ_download('taxonKey = 5229208', "year = 2011"),
+#'   occ_download('taxonKey = 2480946', "year = 2015"),
+#'   occ_download("country = NZ", "year = 1999", "month = 3"),
+#'   occ_download("catalogNumber = Bird.27847588", "year = 1998", "month = 2")
+#' )
+#'
+#' # supports <= 3 requests too
+#' out <- occ_download_queue(
+#'   occ_download("country = NZ", "year = 1999", "month = 3"),
+#'   occ_download("catalogNumber = Bird.27847588", "year = 1998", "month = 2")
+#' )
+#' }
+occ_download_queue <- function(...) {
+  # number of max concurrent requests, has to be hard-coded due to GBIF limits
+  max_concurrent <- 3
+  # set sleep time (seconds) to be used for while loops
+  sleep <- 2
+
+  # collect requests
+  que <- GbifQueue$new(...)
+
+  # stop if no requests submitted
+  if (length(que$reqs) == 0) stop("no requests submitted")
+
+  # initialize bucket to collect data
+  results <- list()
+
+  # add all jobs to the queue
+  que$add_all()
+
+  # start the 1st `max_concurrent` jobs
+  message("kicking off first 3 requests")
+  res <- invisible(lapply(
+    rgbif_compact(que$queue[seq_len(max_concurrent)]), function(x) {
+    # remove from queue
+    que$remove(x)
+    # run job
+    x$run()
+  }))
+  res <- Filter(function(x) inherits(x, "occ_download"), res)
+
+  # handle if 3 requests or less
+  if (que$jobs() == 0) {
+    message("<= 3 requests, waiting for completion")
+    still_running <- TRUE
+    while (still_running) {
+      metas <- lapply(res, occ_download_meta)
+      status <- vapply(metas, "[[", "", "status", USE.NAMES = FALSE)
+      still_running <- !all(tolower(status) %in% c('succeeded', 'killed'))
+      Sys.sleep(sleep)
+    }
+    results <- res
+  } else {
+    message("> 3 requests, waiting for completion")
+    # cycle through until all jobs run and complete
+    while (que$jobs() > 0 || length(res) > 0) {
+      metas <- lapply(res, occ_download_meta)
+      status <- vapply(metas, "[[", "", "status", USE.NAMES = FALSE)
+      statusbool <- tolower(status) %in% c('succeeded', 'killed')
+
+      if (any(statusbool)) {
+        # stash result and drop those done from `res` pool
+        results <- c(results, res[statusbool])
+        res <- res[!statusbool]
+
+        # kick offf new job
+        if (que$jobs() > 0) {
+          ## get job
+          x <- que$queue[1]
+          ## remove from queue
+          que$remove(x[[1]])
+          ## run job
+          res_single <- x[[1]]$run()
+          ## stash into `res` pool
+          res <- c(res, stats::setNames(list(res_single), names(x)))
+        }
+      }
+      Sys.sleep(sleep)
+    }
+  }
+  return(results)
+}
