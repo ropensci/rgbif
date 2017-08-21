2123496d14087ba37b136bfd5cdf4ab05d5bc8d5
Scott Chamberlain <myrmecocystus@gmail.com>
2017-08-21 16:45:15 -0700

    fix #271 fix to gbifmap() to fix cleaning step and correctly remove impossible points also added two checks internally, one that data.frame passed, and one after cleaning in case no results remaining bumped dev version moved templated man docs for gbifmap to the .R file itself

--- R/gbifmap.r
+++ R/gbifmap.r
@@ -1,7 +1,38 @@
 #' Make a map to visualize GBIF occurrence data.
 #'
-#' @template map
 #' @export
+#' @param input Either a single data.frame or a list of data.frame's (e.g.,
+#' from different speies). The data.frame has to have, in addition to any other
+#' columns, columns named exactly "decimalLatitude" and "decimalLongitude".
+#' @param mapdatabase The map database to use in mapping. What you choose here
+#' determines what you can choose in the region parameter. One of: county,
+#' state, usa, world, world2, france, italy, or nz.
+#' @param region The region of the world to map. Run
+#' `sort(unique(ggplot2::map_data("world")$region))` to see region names
+#' for the world database layer, or
+#' `sort(unique(ggplot2::map_data("state")$region))` for the state layer.
+#' @param geom The geom to use, one of geom_point or geom_jitter. Don't
+#' quote them.
+#' @param jitter If you use jitterposition, the amount by which to jitter
+#' points in width, height, or both.
+#' @param customize Further arguments passed on to ggplot.
+#'
+#' @return Map (using ggplot2 package) of points on a map or tiles on a map.
+#'
+#' @details gbifmap takes care of cleaning up the data.frame (removing NA's,
+#' etc.) returned from rgbif functions, and creating the map. This function
+#' gives a simple map of your data.  You can look at the code behing the
+#' function itself if you want to build on it to make a map according
+#' to your specfications.
+#'
+#' Note that this function removes values that are impossible on the globe
+#' (lat values greater than 90 or less than -90; long values greater than 180
+#' or less than -180), and those rows that have both lat and long as NA
+#' or zeros.
+#'
+#' See the `scrubr` package for more powerful and flexible vizualization
+#' options with GBIF data.
+#'
 #' @examples \dontrun{
 #' # Make a map of Puma concolor occurrences
 #' key <- name_backbone(name='Puma concolor')$speciesKey
@@ -33,11 +64,13 @@ gbifmap <- function(input = NULL, mapdatabase = "world", region = ".",
                     geom = geom_point, jitter = NULL, customize = NULL) {
 
   check_for_a_pkg("maps")
+  if (!inherits(input, "data.frame")) stop("'input' must be a data.frame")
   tomap <- input[stats::complete.cases(input$decimalLatitude,
                                        input$decimalLatitude), ]
   tomap <- tomap[!tomap$decimalLongitude == 0 & !tomap$decimalLatitude == 0, ]
-  tomap <- tomap[-(which(tomap$decimalLatitude <= 90 ||
-                           tomap$decimalLongitude <= 180)), ]
+  tomap <- tomap[abs(tomap$decimalLatitude) <= 90 &
+                   abs(tomap$decimalLongitude) <= 180, ]
+  if (NROW(tomap) == 0) stop("after cleaning data, no records remaining")
   tomap$name <- as.factor(gbif_capwords(tomap$name, onlyfirst = TRUE))
 
   if (is.null(jitter)) {
