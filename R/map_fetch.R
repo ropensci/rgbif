07cb53129a34dbb4a333190887d16fc59343b28f
JanLauGe <laurensgeffert@gmail.com>
2018-01-18 22:24:34 +0000

    added parameter documentation

--- R/map_fetch.R
+++ R/map_fetch.R
@@ -1,4 +1,88 @@
-
+#' @title Fetch aggregated density maps of GBIF occurrences
+#'
+#' @export
+#'
+#' @description This function is a wrapper for the GBIF mapping api version 2.0.
+#' The mapping API is a web map tile service making it straightforward to 
+#' visualize GBIF content on interactive maps, and overlay content from other 
+#' sources. It returns tile maps or vector maps with number of 
+#' GBIF records per area unit that can be used in a variety of ways, for example
+#' in interactive leaflet web maps. Map details are specified by a number of 
+#' query parameters, some of them optional. Full documentation of the GBIF
+#' mapping api can be found at https://www.gbif.org/developer/maps
+#'
+#' @param source (character): Either density for fast, precalculated tiles, 
+#' or adhoc for any search. (\code{default = "density"})
+#' 
+#' @param x (integer): the zoom. (\code{default = 0})
+#' 
+#' @param y (integer): the column. (\code{default = 0})
+#' 
+#' @param z (integer): the row. (\code{default = 0})
+#' 
+#' @param format (character): 
+#' .mvt for a vector tile
+#' /@Hx.png for a 256px raster tile (for legacy clients)
+#' /@1x.png for a 512px raster tile
+#' /@2x.png for a 1024px raster tile
+#' /@3x.png for a 2048px raster tile
+#' /@4x.png for a 4096px raster tile
+#' 
+#' @param srs (character):
+#' 	Spatial reference system. One of:
+#' 	EPSG:3857 (Web Mercator)
+#' 	EPSG:4326 (WGS84 plate careé)
+#' 	EPSG:3575 (Arctic LAEA)
+#' 	EPSG:3031 (Antarctic stereographic)
+#' 	see below under Projections.
+#' 	
+#' 	@param bin (optional): square or hex to aggregate occurrence counts into 
+#' 	squares or hexagons. Points by default.
+#' 	
+#' 	@param hexPerTile (optional): sets the size of the hexagons 
+#' 	(the number horizontally across a tile)
+#' 	
+#' 	@param squareSize (optional): sets the size of the squares. Choose a factor 
+#' 	of 4096 so they tessalate correctly: probably from 8, 16, 32, 64, 128, 
+#' 	256, 512.
+#' 	
+#' 	@param style (optional): for raster tiles, choose from the available styles. 
+#' 	Defaults to classic.point.
+#' 	
+#' 	@param search (optional): defines what type of subset of all GBIF data to 
+#' 	return. Should be one of c("taxonKey", "datasetKey", "country", "publisher",
+#' 	"publishingCountry"). Without any search parameter, all occurrences will be 
+#' 	returned.
+#' 	
+#' 	@param id (optional): defines the value to be used as filter criterium in 
+#' 	the category supplied by (\code{search}). Appropriate values depend on the
+#' 	search category that is used, for example integer for 
+#' 	(\code{search = "taxonKey"}). Has to be provided if 
+#' 	(\code{search}) parameter is specified.
+#' 	
+#' 	@param year (optional): limits the occurrences within a range of years, 
+#' 	for example c(2008, 2009, 2010).
+#' 	
+#' 	@param basisOfRecord (optional): can be given multiple times to include 
+#' 	records with that basis of record. The full list is: c("OBSERVATION", 
+#' 	"HUMAN_OBSERVATION", "MACHINE_OBSERVATION", "MATERIAL_SAMPLE", 
+#' 	"PRESERVED_SPECIMEN", "FOSSIL_SPECIMEN", "LIVING_SPECIMEN", 
+#' 	"LITERATURE", "UNKNOWN".
+#' 	
+#' @return ###???
+#'
+#' @details This function uses the arguments passed on to generate a query
+#' to the GBIF web map API. The API returns a web tile object as png that can be
+#' read and converted into an R raster object. The break values or nbreaks
+#' generate a custom colour palette for the web tile, with each bin
+#' corresponding to one grey value. After retrieval, the raster is reclassified
+#' to the actual break values. This is a somewhat hacky but nonetheless
+#' functional solution in the absence of a GBIF raster API implementation.
+#'
+#' @author Laurens Geffert, \email{laurensgeffert@@gmail.com}
+#' @references \url{https://www.gbif.org/developer/maps}
+#' @keywords web map, web tile, GBIF
+#' @examples \dontrun{}
 
 map_fetch <- function(
   source = 'density',
@@ -180,4 +264,5 @@ map_fetch <- function(
   # test response status
   stop_for_status(response)
   
-) {
+  return(response)
+}
