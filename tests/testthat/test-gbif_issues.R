32efa32d12716438e8c563895c691f3c91c45caa
damianooldoni <damiano.oldoni@inbo.be>
2019-01-29 22:36:14 +0100

    Start changing unit-tests

--- tests/testthat/test-gbif_issues.R
+++ tests/testthat/test-gbif_issues.R
@@ -2,12 +2,13 @@ context("gbif_issues")
 
 test_that("gbif_issues", {
   aa <- gbif_issues()
-  
+
   expect_is(aa, "data.frame")
-  expect_named(aa, c('code', 'issue', 'description'))
+  expect_named(aa, c('code', 'issue', 'description', "type"))
   expect_is(aa$code, 'character')
   expect_is(aa$issue, 'character')
   expect_is(aa$description, 'character')
+  expect_is(aa$type, 'character')
 })
 
 test_that("fails correctly", {
