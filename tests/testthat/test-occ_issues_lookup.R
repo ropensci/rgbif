6db886e8330fd67255db98e945d0bb97e6b4ec54
damianooldoni <damiano.oldoni@inbo.be>
2019-01-30 01:55:25 +0100

    Add tests for *_issues_lookup
    Both occ_issues_lookup and name_issues_lookup

--- tests/testthat/test-occ_issues_lookup.R
+++ tests/testthat/test-occ_issues_lookup.R
@@ -14,14 +14,35 @@ test_that("occ_issues_lookup", {
   expect_is(dd, "data.frame")
 
   # returns the correct dimensions
-  expect_equal(NCOL(aa), 3)
-  expect_equal(NCOL(bb), 3)
-  expect_equal(NCOL(cc), 3)
-  expect_equal(NCOL(dd), 3)
+  expect_equal(NCOL(aa), 4)
+  expect_equal(NCOL(bb), 4)
+  expect_equal(NCOL(cc), 4)
+  expect_equal(NCOL(dd), 4)
 })
 
 test_that("fails correctly", {
   skip_on_cran()
 
-  expect_error(occ_issues_lookup(), "invalid 'pattern' argument")
+  expect_error(occ_issues_lookup(),
+               "Invalid pattern argument: issue and code both NULL."
+  )
+  expect_error(occ_issues_lookup(issue = "bad_issue_name"))
+  expect_error(
+    occ_issues_lookup(issue = 'CONTINENT_COUNTRY_MISMATCH',
+                                 code = 'cdiv'),
+    paste("Only one issue or one code allowed. Got: issue",
+          "'CONTINENT_COUNTRY_MISMATCH' and code 'cdiv' at the same time.")
+  )
+  expect_error(
+    occ_issues_lookup(issue = 'BACKBONE_MATCH_NONE'),
+    paste("Issue 'BACKBONE_MATCH_NONE' is not related to occurrences.",
+          "Try name_issues_lookup(issue = 'BACKBONE_MATCH_NONE')."),
+    fixed = TRUE
+  )
+  expect_error(
+    occ_issues_lookup(code = 'bbmn'),
+    paste("Code 'bbmn' is not related to occurrences.",
+          "Try name_issues_lookup(code = 'bbmn')."),
+    fixed = TRUE
+  )
 })
