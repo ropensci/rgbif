4b5b3b09784a4eff5ad164650975f92d84a38658
damianooldoni <damiano.oldoni@inbo.be>
2019-07-18 17:47:11 +0200

    Move tests out of cassette

--- tests/testthat/test-name_issues.R
+++ tests/testthat/test-name_issues.R
@@ -21,35 +21,35 @@ test_that("name_issues", {
 
     ### split and expand
     gg <- out %>% name_issues(mutate = "split_expand")
-
-    # correct class
-    expect_is(aa, "gbif")
-    expect_is(bb, "gbif")
-    expect_is(cc, "gbif")
-    expect_is(dd, "gbif")
-    expect_is(ee, "gbif")
-    expect_is(ff, "gbif")
-    expect_is(gg, "gbif")
-
-    # returns the correct dimensions
-    expect_true(all(vapply(aa$data$issues, function(x) grepl("clasna", x),
-                           logical(1))))
-    expect_true(all(vapply(bb$data$issues, function(x) !grepl("clasna", x),
-                           logical(1))))
-    expect_true(all(vapply(bb$data$issues, function(x) !grepl("bbmn", x),
-                           logical(1))))
-    expect_false(any(grepl("issues", names(cc$data))))
-    expect_true(any(grepl("bbmn", names(cc$data))))
-    expect_false(any(grepl("issues", names(dd$data))))
-    expect_false(any(grepl("scina", names(dd$data))))
-    expect_false(any(grepl("issues", names(ee$data))))
-    expect_true(any(grepl("bbmn", names(ee$data))))
-    expect_true(any(grepl("issues", names(ff$data))))
-    expect_true(any(vapply(ff$data$issues,
-                           function(x) grepl("BACKBONE_MATCH_NONE", x),
-                           logical(1))))
-    expect_false(any(grepl("issues", names(gg$data))))
-    expect_true(any(grepl("CLASSIFICATION_NOT_APPLIED", names(gg$data))))
-
   }, preserve_exact_body_bytes = TRUE)
+
+  # correct class
+  expect_is(aa, "gbif")
+  expect_is(bb, "gbif")
+  expect_is(cc, "gbif")
+  expect_is(dd, "gbif")
+  expect_is(ee, "gbif")
+  expect_is(ff, "gbif")
+  expect_is(gg, "gbif")
+
+  # returns the correct dimensions
+  expect_true(all(vapply(aa$data$issues, function(x) grepl("clasna", x),
+                         logical(1))))
+  expect_true(all(vapply(bb$data$issues, function(x) !grepl("clasna", x),
+                         logical(1))))
+  expect_true(all(vapply(bb$data$issues, function(x) !grepl("bbmn", x),
+                         logical(1))))
+  expect_false(any(grepl("issues", names(cc$data))))
+  expect_true(any(grepl("bbmn", names(cc$data))))
+  expect_false(any(grepl("issues", names(dd$data))))
+  expect_false(any(grepl("scina", names(dd$data))))
+  expect_false(any(grepl("issues", names(ee$data))))
+  expect_true(any(grepl("bbmn", names(ee$data))))
+  expect_true(any(grepl("issues", names(ff$data))))
+  expect_true(any(vapply(ff$data$issues,
+                         function(x) grepl("BACKBONE_MATCH_NONE", x),
+                         logical(1))))
+  expect_false(any(grepl("issues", names(gg$data))))
+  expect_true(any(grepl("CLASSIFICATION_NOT_APPLIED", names(gg$data))))
+
 })
