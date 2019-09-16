4df547728228d976e2b45c60fa78904ef0554e68
Scott Chamberlain <myrmecocystus@gmail.com>
2019-09-16 10:42:27 -0700

    tweak mvt map_fetch eg

--- R/map_fetch.R
+++ R/map_fetch.R
@@ -114,8 +114,7 @@
 #'   # map vector tile, gives back raw bytes
 #'   x <- map_fetch(taxonKey = 2480498, year = 2010,
 #'     format = ".mvt")
-#'   x[1:10]
-#'   is.raw(x)
+#'   x
 #'
 #'   # query with basisOfRecord
 #'   map_fetch(taxonKey = 2480498, year = 2010,
