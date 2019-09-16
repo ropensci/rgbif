c0b2716e96564788cf8563c5d2232410ec24f721
Scott Chamberlain <myrmecocystus@gmail.com>
2019-09-16 10:32:36 -0700

    #373 work on handling mvt files in map_fetch leveraging a new protolite version

--- tests/testthat/test-map_fetch.R
+++ tests/testthat/test-map_fetch.R
@@ -1,12 +1,21 @@
 context("map_fetch")
 
-test_that("map_fetch", {
+test_that("map_fetch - png", {
   skip_on_cran()
 
   x <- map_fetch(taxonKey = 3118771, year = 2010)
   expect_is(x, "RasterLayer")
 })
 
+test_that("map_fetch - mvt", {
+  skip_on_cran()
+
+  x <- map_fetch(taxonKey = 2480498, year = 2002, format = ".mvt")
+  expect_is(x, "list")
+  expect_is(x[[1]], "sf")
+  expect_is(x[[1]], "data.frame")
+  expect_is(x[[1]]$geometry, "sfc_POINT")
+})
 
 test_that("map_fetch fails well", {
   skip_on_cran()
