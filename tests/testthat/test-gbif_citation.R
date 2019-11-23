5647b6ad330d53bc51b3090a4aa97076e64373f8
Scott Chamberlain <myrmecocystus@gmail.com>
2019-11-23 08:20:51 -0800

    preserve_exact_body_bytes for gbif_citation tests - fix #384

--- tests/testthat/test-gbif_citation.R
+++ tests/testthat/test-gbif_citation.R
@@ -2,7 +2,7 @@ test_that("gbif_citation", {
   vcr::use_cassette("gbif_citation", {
     res1 <- occ_search(taxonKey=9206251, limit=2)
     aa <- gbif_citation(res1)
-  })
+  }, preserve_exact_body_bytes = TRUE)
   
   expect_is(aa, "list")
   expect_is(aa[[1]], "gbif_citation")
@@ -21,7 +21,7 @@ test_that("gbif_citation fails correctly", {
   vcr::use_cassette("gbif_citation_error", {
     res3 <- occ_search(taxonKey=9206251,
       fields=c('scientificName','basisOfRecord','protocol'), limit=20)
-  })
+  }, preserve_exact_body_bytes = TRUE)
   expect_error(suppressWarnings(gbif_citation(res3)),
     "No 'datasetKey' or 'key' fields found")
 })
