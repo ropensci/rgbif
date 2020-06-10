4cf59af06ff8d0599eb26bca7c357ed55ed9ecba
Scott Chamberlain <myrmecocystus@gmail.com>
2020-06-10 16:14:17 -0700

    use file within tests/testthat instead of from inst/examples, somehow example file wasnt working on ci

--- tests/testthat/test-occ_download_import.R
+++ tests/testthat/test-occ_download_import.R
@@ -1,10 +1,7 @@
 test_that("occ_download_import", {
   skip_on_cran()
 
-  file <- 
-    system.file("examples/0000066-140928181241064.zip", package = "rgbif")
-
-  dl <- as.download(file)
+  dl <- as.download("0000066-140928181241064.zip")
   expect_is(dl, "occ_download_get")
   z <- occ_download_import(dl, path = tempdir())
   expect_is(z, "tbl_df")
