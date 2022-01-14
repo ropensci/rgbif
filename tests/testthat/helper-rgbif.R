78df155482a2ae07f7f504d97a8d48298047e316
Matthew Blissett <matt@blissett.me.uk>
2022-01-14 14:57:02 +0100

    Remove and exclude irrelevant lines from test fixtures.

--- tests/testthat/helper-rgbif.R
+++ tests/testthat/helper-rgbif.R
@@ -7,5 +7,13 @@ invisible(vcr::vcr_configure(
     "<gbif_pwd>" = Sys.getenv("GBIF_PWD"),
     "<gbif_email>" = Sys.getenv("GBIF_EMAIL"),
     "<geonames_user>" = Sys.getenv("GEONAMES_USER")
+  ),
+  # Exclude irrelevant HTTP request headers
+  vcr_configure(
+    filter_request_headers = c("User-Agent", "X-USER-AGENT")
+  ),
+  # Exclude irrelevant HTTP response headers
+  vcr_configure(
+    filter_response_headers = c("server", "date", "vary", "x-content-type-options", "x-xss-protection", "pragma", "expires", "x-frame-options", "content-length", "cache-control", "x-varnish", "age", "via", "connection", "access-control-allow-origin", "access-control-allow-methods", "transfer-encoding", "accept-ranges")
   )
 ))
