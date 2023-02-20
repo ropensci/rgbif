f103871636cc8c672cdfebac59b02004efc2f607
John Waller <ftw712@ku.dk>
2023-02-20 09:51:01 +0100

    adding tests to occ_data

--- tests/testthat/test-occ_data.R
+++ tests/testthat/test-occ_data.R
@@ -589,6 +589,21 @@ test_that("isInCluster works correctly", {
   expect_equal(tt$data$classKey[1], 212)
 })
 
+# Test argument distanceFromCentroidInMeters
+test_that("distanceFromCentroidInMeters works correctly", {
+  skip_on_cran() # because fixture in .Rbuildignore
+  vcr::use_cassette("occ_data_distanceFromCentroidInMeters", {
+    ee <- occ_data(distanceFromCentroidInMeters=0,limit=2)
+    rr <- occ_data(distanceFromCentroidInMeters="2000,*",limit=2)
+    dd <- occ_data(distanceFromCentroidInMeters="0,2000",limit=2)
+  }, preserve_exact_body_bytes = TRUE)
+  
+  expect_equal(nrow(ee$data),2)
+  expect_equal(nrow(rr$data),2)
+  expect_equal(nrow(dd$data),2)
+  expect_true(rr$meta$count > dd$meta$count)
+})
+
 
 # Get occurrences for with a particular networkKey
 test_that("networkKey works correctly", {
