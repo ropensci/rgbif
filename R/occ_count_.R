bcbfc95ab1a879a2fbc3ead8c936e19029758b2a
John Waller <ftw712@ku.dk>
2023-03-06 14:40:08 +0100

    changing occ_counts_ to occ_count_

--- R/occ_count_.R
+++ R/occ_count_.R
@@ -11,19 +11,19 @@
 #' @details 
 #' Get quick pre-computed counts of a limited number of dimensions. 
 #' 
-#' `occ_counts_country()` will return a data.frame with occurrence counts by 
-#' country. By using `occ_counts_country(publishingCountry="DK")` will
+#' `occ_count_country()` will return a data.frame with occurrence counts by 
+#' country. By using `occ_count_country(publishingCountry="DK")` will
 #' return the occurrence contributions Denmark has made to each country.
 #' 
-#' `occ_counts_pub_country()` will return a data.frame with occurrence counts by 
-#' publishing country. Using `occ_counts_pub_country(country="DK")`, will return
+#' `occ_count_pub_country()` will return a data.frame with occurrence counts by 
+#' publishing country. Using `occ_count_pub_country(country="DK")`, will return
 #' the occurrence contributions each country has made to that focal `country=DK`.  
 #'   
-#' `occ_counts_year()` will return a data.frame with the total occurrences 
+#' `occ_count_year()` will return a data.frame with the total occurrences 
 #' mediated by GBIF for each year. By using `occ_counts_year(year="1800,1900")`
 #' will only return counts for that range.
 #' 
-#' `occ_counts_basis_of_record()` will return a data.frame with total occurrences
+#' `occ_count_basis_of_record()` will return a data.frame with total occurrences
 #' mediated by GBIF for each basis of record. 
 #' 
 #' @return
@@ -31,30 +31,30 @@
 #'
 #' @examples \dontrun{
 #' # total occurrence counts for all countries and iso2 places
-#' occ_counts_country()  
+#' occ_count_country()  
 #' # the occurrences Mexico has published in other countries 
-#' occ_counts_country("MX") 
+#' occ_count_country("MX") 
 #' # the occurrences Denmark has published in other countries 
-#' occ_counts_country("DK")
+#' occ_count_country("DK")
 #' 
 #' # the occurrences other countries have published in Denmark
-#' occ_counts_pub_country("DK")
+#' occ_count_pub_country("DK")
 #' # the occurrences other countries have published in Mexico
-#' occ_counts_pub_country("MX")
+#' occ_count_pub_country("MX")
 #' 
 #' # total occurrence counts for each year that an occurrence was 
 #' # recorded or collected.
-#' occ_counts_year()
+#' occ_count_year()
 #' # supports ranges
-#' occ_counts_year("1800,1900")
+#' occ_count_year("1800,1900")
 #' 
 #' # table of occurrence counts by basis of record
-#' occ_counts_basis_of_record()
+#' occ_count_basis_of_record()
 #' 
 #' }
 #' @export
-#' @rdname occ_counts_
-occ_counts_country <- function(publishingCountry = NULL) {
+#' @rdname occ_count_
+occ_count_country <- function(publishingCountry = NULL) {
   assert(publishingCountry,"character")
   url <- paste0(gbif_base(),'/occurrence/counts/countries')
   args <- rgbif_compact(list(publishingCountry=publishingCountry))
@@ -67,8 +67,8 @@ occ_counts_country <- function(publishingCountry = NULL) {
 }
 
 #' @export
-#' @rdname occ_counts_
-occ_counts_pub_country <- function(country = NULL) {
+#' @rdname occ_count_
+occ_count_pub_country <- function(country = NULL) {
   assert(country,"character")
   if(is.null(country)) stop("Supply a iso2 countrycode.")
   url <- paste0(gbif_base(),'/occurrence/counts/publishingCountries')
@@ -81,8 +81,8 @@ occ_counts_pub_country <- function(country = NULL) {
 }
 
 #' @export
-#' @rdname occ_counts_
-occ_counts_year <- function(year=NULL) {
+#' @rdname occ_count_
+occ_count_year <- function(year=NULL) {
   url <- paste0(gbif_base(),'/occurrence/counts/year')
   res <- tibble::as_tibble(gbif_GET(url,args=list(year=year)))
   res <- data.table::transpose(res,keep.names="x")
@@ -91,8 +91,8 @@ occ_counts_year <- function(year=NULL) {
 }
 
 #' @export
-#' @rdname occ_counts_
-occ_counts_basis_of_record <- function(curlopts=list()) {
+#' @rdname occ_count_
+occ_count_basis_of_record <- function(curlopts=list()) {
   url <- paste0(gbif_base(),'/occurrence/counts/basisOfRecord')
   res <- tibble::as_tibble(gbif_GET(url,args=NULL))
   res <- data.table::transpose(res,keep.names="x")
--- tests/testthat/test-occ_count_.R
+++ tests/testthat/test-occ_count_.R
@@ -1,13 +1,13 @@
-context("occ_counts_")
+context("occ_count_")
 
-test_that("occ_counts_", {
-  vcr::use_cassette("occ_counts_", {
-    aa <- occ_counts_country()
-    bb <- occ_counts_country(publishingCountry="DK")
-    cc <- occ_counts_pub_country(country="DK")
-    dd <- occ_counts_year()
-    ee <- occ_counts_year(year="1990,2000")
-    ff <- occ_counts_basis_of_record()
+test_that("occ_count_ custom functions work", {
+  vcr::use_cassette("occ_count_", {
+    aa <- occ_count_country()
+    bb <- occ_count_country(publishingCountry="DK")
+    cc <- occ_count_pub_country(country="DK")
+    dd <- occ_count_year()
+    ee <- occ_count_year(year="1990,2000")
+    ff <- occ_count_basis_of_record()
   })
   
   # returns the correct class
@@ -34,13 +34,13 @@ test_that("occ_counts_", {
 
 })
 
-test_that("occ_counts_ fails well", {
+test_that("occ_count_ fails well", {
   
-  expect_error(occ_counts_pub_country(), 
+  expect_error(occ_count_pub_country(), 
     "Supply a iso2 countrycode.")           
-  expect_error(occ_counts_pub_country(1),
+  expect_error(occ_count_pub_country(1),
     "country must be of class character")
-  expect_error(occ_counts_country(1),
+  expect_error(occ_count_country(1),
     "publishingCountry must be of class character")
   
 })
