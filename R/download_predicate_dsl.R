6cab38f0624bc3cf6705b77e4cb950070f4f993e
John Waller <ftw712@ku.dk>
2024-04-08 19:58:16 +0200

    adding support for verbatim_extensions

--- R/download_predicate_dsl.R
+++ R/download_predicate_dsl.R
@@ -278,6 +278,13 @@ pred_default <- function() {
   pred_not(pred_in("BASIS_OF_RECORD",c("FOSSIL_SPECIMEN","LIVING_SPECIMEN")))
   )
 }
+#' @rdname download_predicate_dsl
+#' @export
+verbatim_extensions <- function(...) {
+  x <- unlist(list(...))
+  assert(x,"character")
+  structure(x, class = "verbatim_extensions")
+}
 
 #' @export
 print.occ_predicate <- function(x, ...) {
@@ -295,6 +302,15 @@ print.occ_predicate_list <- function(x, ...) {
     } else {
      cat("  ", pred_cat(i), "\n", sep = "")
     }
+  }
+}
+
+#' @export
+print.verbatim_extensions <- function(...) {
+  x <- unlist(list(...))
+  cat("<<gbif download - verbatim_extensions>>", sep = "\n")
+  for(i in seq_along(x)) {
+  cat("  ", x[i], "\n", sep = "")
   }
 }
 
@@ -580,7 +596,7 @@ sub_str <- function(str, max = 100) {
 parse_predicates <- function(user, email, type, format, ...) {
   tmp <- list(...)
   clzzs <- vapply(tmp,
-    function(z) inherits(z, c("occ_predicate", "occ_predicate_list")),
+    function(z) inherits(z, c("verbatim_extensions","occ_predicate", "occ_predicate_list")),
     logical(1)
   )
   if (!all(clzzs))
@@ -592,6 +608,20 @@ parse_predicates <- function(user, email, type, format, ...) {
     format = unbox(format),
     predicate = list()
   )
+  # handle verbatim_extensions 
+  if(any(sapply(tmp,class) %in% "verbatim_extensions")) {
+    if(!format == "DWCA") stop("verbatim_extensions only work with format='DWCA'")
+    ve_idx <- which(sapply(tmp,class) == "verbatim_extensions")
+    if(!length(ve_idx) == 1) stop("Please supply only one verbatim_extensions() expression.")
+    payload <- list(
+      creator = unbox(user),
+      notification_address = email,
+      format = unbox(format),
+      predicate = list(),
+      verbatimExtensions = unclass(tmp[[ve_idx]])
+    )
+    tmp[ve_idx] <- NULL
+  }
   if (any(vapply(tmp, function(w) "predicates" %in% names(w), logical(1)))) {
     payload$predicate <- list(unclass(tmp[[1]]))
   } else {
